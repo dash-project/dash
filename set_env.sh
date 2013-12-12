64d326effd185cce53fd1b0c80eba6a86fb627e3
Michael Maier <maierm@nm.ifi.lmu.de>
2013-12-12 12:57:07 +0100

    - spec-folder -> dart-impl/dummy - added gtest-1.6.0.zip - set_env.sh now deploys gtest-lib

--- set_env.sh
+++ set_env.sh
@@ -11,6 +11,28 @@ fi
 export DASH_HOME=`pwd`
 export DASH_INCLUDES=$PARENT_DIR/include
 export DASH_LIBS=$PARENT_DIR/lib
+mkdir -p $DASH_INCLUDES
+mkdir -p $DASH_LIBS
 export CPATH=$DASH_INCLUDES
 export LD_LIBRARY_PATH=$DASH_LIBS
 
+if [ ! -d "$PARENT_DIR/gtest" ]; then
+    echo ' '
+    echo "---> unzipping google test lib into $PARENT_DIR"
+    echo ' '
+    unzip gtest-1.6.0.zip -d $PARENT_DIR
+    echo ' '
+    echo "---> configure"
+    echo ' '
+    cd $PARENT_DIR/gtest-1.6.0 && ./configure 
+    echo ' '
+    echo "---> calling make"
+    echo ' '
+    make 
+    cd -
+    echo ' '
+    echo "---> copying generated files..."
+    echo ' '
+    cp -r $PARENT_DIR/gtest-1.6.0/include/gtest $DASH_INCLUDES
+    cp $PARENT_DIR/gtest-1.6.0/lib/.libs/libgtest.a $DASH_LIBS
+fi
--- spec/dart-dummy/dart_gptr_impl.h
+++
@@ -1,7 +0,0 @@
-#ifndef DART_GPTR_IMPL_H_INCLUDED
-#define DART_GPTR_IMPL_H_INCLUDED
-
-#include "dart_globmem.h"
-
-
-#endif /* DART_GPTR_IMPL_H_INCLUDED */
