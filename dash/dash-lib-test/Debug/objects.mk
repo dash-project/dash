462933eef431cfe2e8593b7ac6b46cb49679152a
Michael Maier <maierm@nm.ifi.lmu.de>
2013-12-12 13:35:50 +0100

    eclipse-build now unsupported. It should be no problem to import these projects in eclipse...

--- dash/dash-lib-test/Debug/objects.mk
+++
@@ -1,8 +0,0 @@
-################################################################################
-# Automatically-generated file. Do not edit!
-################################################################################
-
-USER_OBJS :=
-
-LIBS := -ldash -ldart-shmem-base -ldart-shmem-sysv -lgtest -lpthread
-
