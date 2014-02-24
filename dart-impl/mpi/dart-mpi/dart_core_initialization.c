bc403bcc701f0348fe3aff6fccc633ff36331741
Yousri Mhedheb <yousri@linux-SuseVM.site>
2014-02-24 19:20:05 +0100

    core files are changed and libdart.o compiled

--- dart-impl/mpi/dart-mpi/dart_core_initialization.c
+++ dart-impl/mpi/dart-mpi/dart_core_initialization.c
@@ -5,7 +5,7 @@
 
 dart_ret_t dart_init (int* argc, char*** argv)
 {
-           return dart_adapt_init (int* argc, char*** argv);	
+           return dart_adapt_init (argc, argv);	
 }
 
 dart_ret_t dart_exit ()
