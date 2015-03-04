37e9b71b7e0287b907e1a33097eaddd3d1365422
Karl Fuerlinger <Karl.Fuerlinger@nm.ifi.lmu.de>
2015-03-04 14:27:37 +0100

    Implementing

--- dash/dash-lib/Team.h
+++ dash/dash-lib/Team.h
@@ -147,9 +147,59 @@ public:
   static Team& Null() {
     return m_team_null;
   }
-
-  Team& split(unsigned n);
-
+  
+  Team& split(unsigned nParts) {
+    dart_group_t *group;
+    dart_group_t *gout[nParts];
+    size_t size;
+    char buf[200];
+    
+    dart_group_sizeof(&size);
+    
+    group = (dart_group_t*) malloc(size);
+    for(int i=0; i<nParts; i++ ) {
+      gout[i] = (dart_group_t*) malloc(size);
+    }
+    
+    Team *result = &(dash::Team::Null());
+    
+    if( this->size()<=1 ) {
+      return *result;
+    }
+    
+    dart_group_init(group);
+    dart_team_get_group(m_dartid, group);
+    dart_group_split(group, nParts, gout);
+    /*
+      GROUP_SPRINTF(buf, group);
+      fprintf(stderr, "[%d]: Splitting team[%d]: %s\n", 
+      dash::myId(), m_dartid, buf);
+    */
+    
+    dart_team_t oldteam = m_dartid;
+    
+    for(int i=0; i<nParts; i++) {
+      dart_team_t newteam;
+      
+      dart_team_create(oldteam, gout[i], &newteam);
+      
+      /*
+	GROUP_SPRINTF(buf, gout[i]);
+	fprintf(stderr, "[%d] New subteam[%d]: %s\n", 
+	myId(), newteam, buf);
+      */
+      if(newteam!=DART_TEAM_NULL) {
+	result=new Team(newteam, this, i);
+      }
+      
+      /*    
+	    GROUP_SPRINTF(buf, gout[i]);
+	    fprintf(stderr, "%d: %s\n", i, buf);
+      */
+    }
+    return *result;
+  }
+    
   bool operator==(const Team& rhs) const {
     return m_dartid==rhs.m_dartid;
   }
@@ -239,7 +289,6 @@ public:
 		       &globalid);
     
     return globalid;
-    
   }
 
 };
@@ -257,7 +306,11 @@ public:
 template<int DIM>
 using TeamView = CartView<Team::iterator, DIM>;
 
+
 } // namespace dash
+
+
+
 
 namespace std {
 
@@ -270,6 +323,8 @@ public:
   typedef dash::Team::iterator difference_type;
   typedef random_access_iterator_tag iterator_category;
 };
+
+
 
 
 } // namespace std
