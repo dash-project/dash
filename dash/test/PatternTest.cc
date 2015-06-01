1fdff29b00890a8cb0cded7385d500e148bcd54f
Tobias Fuchs <fuchst@nm.ifi.lmu.de>
2015-06-01 19:36:50 +0200

    Extending unit tests of dash::Pattern

--- dash/test/PatternTest.cc
+++ dash/test/PatternTest.cc
@@ -2,11 +2,22 @@
 #include "PatternTest.h"
 
 TEST_F(PatternTest, DistributeDim1) {
-  dash::Pattern<1> pat(
+  int team_size  = dash::Team::All().size();
+  int block_size = ceil(_num_elem / team_size);
+  dash::Pattern<1> pat_blocked(
       dash::SizeSpec<1, dash::ROW_MAJOR>(_num_elem),
       dash::DistributionSpec<1>(dash::BLOCKED),
       dash::TeamSpec<1>(),
       dash::Team::All());
-  // First element -> first unit
-  EXPECT_EQ(pat.atunit(0), 0);
+  EXPECT_EQ(pat_blocked.blocksize(1), block_size);
+  // First element -> unit 0
+  EXPECT_EQ(
+      pat_blocked.index_to_unit(std::array<long long, 1> {0}),
+      0);
+  EXPECT_EQ(
+      pat_blocked.index_to_unit(std::array<long long, 1> {block_size-1}),
+      0);
+  EXPECT_EQ(
+      pat_blocked.index_to_unit(std::array<long long, 1> {block_size}),
+      1);
 }
