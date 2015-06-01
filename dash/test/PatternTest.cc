7ec39b983146c2c002514d9742148e23bf5df2f0
Tobias Fuchs <fuchst@nm.ifi.lmu.de>
2015-06-01 19:41:03 +0200

    Added (failing) assertions in dash::Pattern unit tests

--- dash/test/PatternTest.cc
+++ dash/test/PatternTest.cc
@@ -9,14 +9,19 @@ TEST_F(PatternTest, DistributeDim1) {
       dash::DistributionSpec<1>(dash::BLOCKED),
       dash::TeamSpec<1>(),
       dash::Team::All());
+  // Fails:
+  EXPECT_EQ(pat_blocked.nelem(), _num_elem);
   EXPECT_EQ(pat_blocked.blocksize(1), block_size);
   // First element -> unit 0
   EXPECT_EQ(
       pat_blocked.index_to_unit(std::array<long long, 1> {0}),
       0);
+  // Last element in first block -> unit 0
   EXPECT_EQ(
       pat_blocked.index_to_unit(std::array<long long, 1> {block_size-1}),
       0);
+  // First element in second block -> unit 1
+  // Fails:
   EXPECT_EQ(
       pat_blocked.index_to_unit(std::array<long long, 1> {block_size}),
       1);
