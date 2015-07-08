bbebcce5ad0e52f5b45f35f31031526caaa5b8f6
Karl Fuerlinger <Karl.Fuerlinger@nm.ifi.lmu.de>
2015-07-08 20:38:45 +0200

    simplified hello world example and moved MPI related stuff into separate hello-mpi example

---
+++ dash/examples/test.01.hello-mpi/main.cpp
@@ -0,0 +1,73 @@
+/* 
+ * hello/main.cpp 
+ *
+ * author(s): Karl Fuerlinger, LMU Munich */
+/* @DASH_HEADER@ */
+
+#include <unistd.h>
+#include <iostream>
+#include <cstddef>
+
+#include <libdash.h>
+
+#ifdef DART_MPI
+#include "mpi.h"
+#endif
+
+//#define INIT_TEST
+
+using namespace std;
+
+int main(int argc, char* argv[])
+{
+  pid_t pid;
+  char buf[100];
+
+#if defined(DART_MPI) && defined(INIT_TEST)
+  MPI_Init(&argc, &argv);
+#endif
+
+  dash::init(&argc, &argv);
+  
+  auto myid = dash::myid();
+  auto size = dash::size();
+
+  gethostname(buf, 100);
+  pid = getpid();
+
+  if(myid==0 ) {
+    cout<<"-------------------------"<<endl;
+#ifdef DART_MPI
+    cout<<argv[0]<<" "<<"built with DART_MPI"<<endl;
+#endif 
+#ifdef DART_SHMEM
+    cout<<argv[0]<<" "<<"built with DART_SHMEM"<<endl;
+#endif 
+    
+#ifdef MPI_VERSION
+    cout<<"-------------------------"<<endl;
+    cout<<"MPI_VERSION    : "<<MPI_VERSION<<endl;
+    cout<<"MPI_SUBVERSION : "<<MPI_SUBVERSION<<endl;
+#ifdef MPICH
+    cout<<"MPICH          : "<<MPICH<<endl;
+    cout<<"MPICH_NAME     : "<<MPICH_NAME<<endl;
+    cout<<"MPICH_HAS_C2F  : "<<MPICH_HAS_C2F<<endl;
+#endif // MPICH
+#ifdef OPEN_MPI
+    cout<<"OPEN_MPI       : "<<OPEN_MPI<<endl;
+#endif // OPEN_MPI
+#endif // MPI_VERSION
+    cout<<"-------------------------"<<endl;
+  }
+  
+  dash::barrier();
+  
+  cout<<"'Hello world' from unit "<<myid<<
+    " of "<<size<<" on "<<buf<<" pid="<<pid<<endl;
+  
+  dash::finalize();
+
+#if defined(DART_MPI) && defined(INIT_TEST)
+  MPI_Finalize();
+#endif
+}
--- dash/examples/test.01.hello/main.cpp
+++ dash/examples/test.01.hello/main.cpp
@@ -10,10 +10,6 @@
 
 #include <libdash.h>
 
-#ifdef DART_MPI
-#include "mpi.h"
-#endif
-
 using namespace std;
 
 int main(int argc, char* argv[])
@@ -29,33 +25,6 @@ int main(int argc, char* argv[])
   gethostname(buf, 100);
   pid = getpid();
 
-  if(myid==0 ) {
-    cout<<"-------------------------"<<endl;
-#ifdef DART_MPI
-    cout<<argv[0]<<" "<<"built with DART_MPI"<<endl;
-#endif 
-#ifdef DART_SHMEM
-    cout<<argv[0]<<" "<<"built with DART_SHMEM"<<endl;
-#endif 
-    
-#ifdef MPI_VERSION
-    cout<<"-------------------------"<<endl;
-    cout<<"MPI_VERSION    : "<<MPI_VERSION<<endl;
-    cout<<"MPI_SUBVERSION : "<<MPI_SUBVERSION<<endl;
-#ifdef MPICH
-    cout<<"MPICH          : "<<MPICH<<endl;
-    cout<<"MPICH_NAME     : "<<MPICH_NAME<<endl;
-    cout<<"MPICH_HAS_C2F  : "<<MPICH_HAS_C2F<<endl;
-#endif // MPICH
-#ifdef OPEN_MPI
-    cout<<"OPEN_MPI       : "<<OPEN_MPI<<endl;
-#endif // OPEN_MPI
-#endif // MPI_VERSION
-    cout<<"-------------------------"<<endl;
-  }
-  
-  dash::barrier();
-  
   cout<<"'Hello world' from unit "<<myid<<
     " of "<<size<<" on "<<buf<<" pid="<<pid<<endl;
   
