4944555e3a3c4474750f6aca1487ec748d016dec
Tobias Fuchs <tobias.fuchs@nm.ifi.lmu.de>
2015-08-12 01:00:03 +0200

    Fixed doxygen documentation of Pattern concept

--- dash/include/dash/Pattern.h
+++ dash/include/dash/Pattern.h
@@ -18,19 +18,20 @@ namespace dash {
  * A pattern realizes a projection of a global index range to a
  * local view:
  *
- * Distribution                 | Container                     |
+ * Team Spec                    | Container                     |
  * ---------------------------- | ----------------------------- |
- * <tt>[ team 0 : team 1 ]</tt> | <tt>[ 0  1  2  3  4  5 ]</tt> |
- * <tt>[ team 1 : team 0 ]</tt> | <tt>[ 6  7  8  9 10 11 ]</tt> |
+ * <tt>[ unit 0 : unit 1 ]</tt> | <tt>[ 0  1  2  3  4  5 ]</tt> |
+ * <tt>[ unit 1 : unit 0 ]</tt> | <tt>[ 6  7  8  9 10 11 ]</tt> |
  *
  * This pattern would assign local indices to teams like this:
  * 
  * Team            | Local indices                 |
  * --------------- | ----------------------------- |
- * <tt>team 0</tt> | <tt>[ 0  1  2  9 10 11 ]</tt> |
- * <tt>team 1</tt> | <tt>[ 3  4  5  6  7  8 ]</tt> |
+ * <tt>unit 0</tt> | <tt>[ 0  1  2  9 10 11 ]</tt> |
+ * <tt>unit 1</tt> | <tt>[ 3  4  5  6  7  8 ]</tt> |
  *
  * \par Methods
+ *
  * <table>
  *   <tr>
  *     <th>Method Signature</th>
@@ -39,7 +40,9 @@ namespace dash {
  *
  *   <tr>
  *     <td>
- *       \c Index[D] coords(Index gi)
+ *       \code
+ *       Index[D] coords(Index gi)
+ *       \endcode
  *     </td>
  *     <td>
  *       Global linear offset to global cartesian coordinates
@@ -48,7 +51,9 @@ namespace dash {
  *
  *   <tr>
  *     <td>
- *       \c Index at(Index[D] gp)
+ *       \code
+ *       Index at(Index[D] gp)
+ *       \endcode
  *     </td>
  *     <td>
  *       Global point to local linear offset
@@ -57,7 +62,9 @@ namespace dash {
  *
  *   <tr>
  *     <td>
- *       \c Index local_at(Index[D] lp)
+ *       \code
+ *       Index local_at(Index[D] lp)
+ *       \endcode
  *     </td>
  *     <td>
  *       Lobal point to local linear offset
@@ -66,7 +73,9 @@ namespace dash {
  *
  *   <tr>
  *     <td>
- *       \c { dart_unit_t u, Index li } local(Index[D] gp)
+ *       \code
+ *       { dart_unit_t u, Index li } local(Index[D] gp)
+ *       \endcode
  *     </td>
  *     <td>
  *       Global point to unit and local linear offset, inverse of
@@ -76,7 +85,9 @@ namespace dash {
  *
  *   <tr>
  *     <td>
- *       \c Index[D] global(dart_unit_t u, Index gi)
+ *       \code
+ *       Index[D] global(dart_unit_t u, Index gi)
+ *       \endcode
  *     </td>
  *     <td>
  *       Unit and lobal linear offset to global cartesian coordinates,
@@ -86,7 +97,9 @@ namespace dash {
  *
  *   <tr>
  *     <td>
- *       \c bool is_local(Index gi, dart_unit_t u)
+ *       \code
+ *       bool is_local(Index gi, dart_unit_t u)
+ *       \endcode
  *     </td>
  *     <td>
  *       Whether element at global index \gi is local to unit \c u
@@ -95,7 +108,9 @@ namespace dash {
  *
  *   <tr>
  *     <td>
- *       \c bool is_local(dim_t d, Index go, dart_unit_t u)
+ *       \code
+ *       bool is_local(dim_t d, Index go, dart_unit_t u)
+ *       \endcode
  *     </td>
  *     <td>
  *       Whether any element in dimension \c d at global offset \go (such
@@ -105,7 +120,9 @@ namespace dash {
  *
  *   <tr>
  *     <td>
- *       \c Size capacity()
+ *       \code
+ *       Size capacity()
+ *       \endcode
  *     </td>
  *     <td>
  *       Size of the cartesian index space, total number of elements in
@@ -115,7 +132,9 @@ namespace dash {
  *
  *   <tr>
  *     <td>
- *       \c Size local_capacity()
+ *       \code
+ *       Size local_capacity()
+ *       \endcode
  *     </td>
  *     <td>
  *       Maximum size of the local cartesian index space for any unit, i.e.
@@ -125,7 +144,9 @@ namespace dash {
  *
  *   <tr>
  *     <td>
- *       \c Size size()
+ *       \code
+ *       Size size()
+ *       \endcode
  *     </td>
  *     <td>
  *       Maximum size of the local cartesian index space for any unit, i.e.
@@ -136,7 +157,9 @@ namespace dash {
  *
  *   <tr>
  *     <td>
- *       \c Size local_size()
+ *       \code
+ *       Size local_size()
+ *       \endcode
  *     </td>
  *     <td>
  *       Actual number of elements assigned to the active unit
@@ -145,7 +168,9 @@ namespace dash {
  *
  *   <tr>
  *     <td>
- *       \c Size extent(dim_t d)
+ *       \code
+ *       Size extent(dim_t d)
+ *       \endcode
  *     </td>
  *     <td>
  *       Number of elements in the pattern in dimension \c d
@@ -154,7 +179,9 @@ namespace dash {
  *
  *   <tr>
  *     <td>
- *       \c Size local_extent(dim_t d)
+ *       \code
+ *       Size local_extent(dim_t d)
+ *       \endcode
  *     </td>
  *     <td>
  *       Number of elements in the pattern in dimension \c d that are local
