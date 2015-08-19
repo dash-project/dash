8f7a5909f3e3adaf766db9ae7934945894fb0ba6
Tobias Fuchs <fuchst@nm.ifi.lmu.de>
2015-08-20 00:15:57 +0200

    Revert "Implement the interface of"
    This reverts commit fb63f4c10904ed26a37232868023224e2de787ac.

--- dart-impl/mpi/include/dash/dart/mpi/dart_globmem_priv.h
+++ dart-impl/mpi/include/dash/dart/mpi/dart_globmem_priv.h
@@ -1,5 +1,4 @@
 extern int16_t dart_memid;
-extern int16_t dart_registermemid;
 #define DART_GPTR_COPY(gptr_, gptrt_)                           \
 	({gptr_.addr_or_offs.offset = gptrt_.addr_or_offs.offset;	\
 	gptr_.flags = gptrt_.flags;					\
