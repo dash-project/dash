afa73785a625e97eaf283120c79ed8cd4a1f8c5a
Tobias Fuchs <fuchst@nm.ifi.lmu.de>
2015-10-02 03:10:22 +0200

    Imlementing local-to-global dash::copy

--- dash/include/dash/algorithm/Copy.h
+++ dash/include/dash/algorithm/Copy.h
@@ -219,8 +219,7 @@ GlobOutputIt copy(
     ValueType * l_out_first = ((GlobPtr<ValueType>)
                                (out_first + l_elem_offset));
     DASH_LOG_TRACE_VAR("dash::copy", l_out_first);
-    ValueType * l_out_last  = ((GlobPtr<ValueType>)
-                               (out_first + l_elem_offset - 1)) + 1;
+    ValueType * l_out_last  = l_out_first + l_range_size;
     DASH_LOG_TRACE_VAR("dash::copy", l_out_last);
     // ... [ ........ | ---- l ---- | ......... ] ...
     //     ^          ^             ^           ^
@@ -232,20 +231,20 @@ GlobOutputIt copy(
     DASH_ASSERT(
       std::copy(in_first + l_elem_offset,
                 in_first + l_elem_offset + l_range_size,
-                l_out_first + l_g_offset_begin) == l_out_last);
+                l_out_first) == l_out_last);
     // Copy to remote elements preceding the local subrange:
     if (l_g_offset_begin > out_first.pos()) {
       DASH_LOG_TRACE("dash::copy", "copy to global preceding local subrange");
       out_last = dash::internal::copy_impl(in_first,
-                                           in_first + l_range_size,
-                                           out_first + l_g_offset_begin);
+                                           in_first + l_elem_offset,
+                                           out_first);
     }
     // Copy to remote elements succeeding the local subrange:
     if (l_g_offset_end < out_h_last.pos()) {
       DASH_LOG_TRACE("dash::copy", "copy to global succeeding local subrange");
-      out_last = dash::internal::copy_impl(in_first + li_range_out.end,
+      out_last = dash::internal::copy_impl(in_first + l_elem_offset + l_range_size,
                                            in_last,
-                                           out_first + l_g_offset_begin);
+                                           out_first + l_range_size);
     }
   } else {
     // All elements in output range are remote
