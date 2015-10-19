48c8d0866bb3278f2e83828803843442368b7620
Tobias Fuchs <fuchst@nm.ifi.lmu.de>
2015-10-20 01:34:32 +0200

    Added developer build script

---
+++ build-dev.sh
@@ -0,0 +1,37 @@
+#!/bin/sh
+
+FORCE_BUILD=false
+if [ "$1" = "-f" ]; then
+  FORCE_BUILD=true
+fi
+
+await_confirm() {
+  if ! $FORCE_BUILD; then
+    echo ""
+    read -p "   To build using these settings, hit ENTER"
+  fi
+}
+
+exit_message() {
+  echo "----------------------------------------------------"
+  echo "Done. To install DASH, run  make install  in ./build"
+}
+
+mkdir -p build
+rm -Rf ./build/*
+(cd ./build && cmake -DCMAKE_BUILD_TYPE=Debug \
+                     -DENABLE_ASSERTIONS=ON \
+                     -DDART_IF_VERSION=3.2 \
+                     -DINSTALL_PREFIX=$HOME/opt/ \
+                     -DDART_IMPLEMENTATIONS=mpi,shmem \
+                     -DBUILD_EXAMPLES=ON \
+                     -DENABLE_LOGGING=ON \
+                     -DENABLE_TRACE_LOGGING=ON \
+                     -DENABLE_DART_LOGGING=ON \
+                     -DMEMORY_MODEL_UNIFIED=ON \
+                     -DBUILD_TESTS=ON \
+                     -DPAPI_PREFIX=${PAPI_HOME} \
+                     ../ && \
+ await_confirm && \
+ make) && \
+exit_message
