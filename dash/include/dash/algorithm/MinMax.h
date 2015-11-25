39c4adacd683f352adc55021d3f7e2f3bf82c134
Tobias Fuchs <fuchst@nm.ifi.lmu.de>
2015-11-25 02:13:15 +0100

    dash::min_element, dash::max_element: Added log messages, added specialization for native pointer ranges

--- dash/include/dash/algorithm/MinMax.h
+++ dash/include/dash/algorithm/MinMax.h
@@ -5,6 +5,8 @@
 #include <dash/algorithm/LocalRange.h>
 #include <dash/internal/Logging.h>
 
+#include<algorithm>
+
 namespace dash {
 
 /**
@@ -36,7 +38,7 @@ GlobPtr<ElementType> min_element(
   typedef dash::GlobPtr<ElementType> globptr_t;
   auto pattern      = first.pattern();
   dash::Team & team = pattern.team();
-  DASH_LOG_DEBUG("min_element",
+  DASH_LOG_DEBUG("min_element()",
                  "allocate minarr, size", team.size());
   dash::Array<globptr_t> minarr(team.size());
   // return last for empty array
@@ -54,43 +56,85 @@ GlobPtr<ElementType> min_element(
     DASH_LOG_DEBUG("min_element", "local range empty");
   } else {
     const ElementType * lmin = 
-      ::std::min_element(lbegin, lend, compare);     
+      ::std::min_element(lbegin, lend, compare);
+    // Local index of local minimum:
+    auto lidx_lmin = lmin - lbegin;
     DASH_LOG_TRACE_VAR("min_element", lend - lbegin);
-    DASH_LOG_TRACE_VAR("min_element", lmin - lbegin);
+    DASH_LOG_TRACE_VAR("min_element", lidx_lmin);
     DASH_LOG_DEBUG_VAR("min_element", *lmin);
-    minarr[team.myid()] = first.globmem().index_to_gptr(
-                            team.myid(),
-                            lmin - lbegin);
+    // Global pointer to local minimum:
+    auto gptr_lmin = first.globmem().index_to_gptr(
+                                       team.myid(),
+                                       lidx_lmin);
+    DASH_LOG_DEBUG_VAR("min_element", gptr_lmin);
+    minarr[team.myid()] = gptr_lmin;
   }
+  DASH_LOG_TRACE("min_element", "Waiting for local min of other units");
   team.barrier();
+  // Shared global pointer referencing element with global minimum:
   dash::Shared<globptr_t> min; 
-  // find the global min. element
+  // Find the global min. element:
   if (team.myid() == 0) {
-    globptr_t minloc   = minarr[0];
+    DASH_LOG_TRACE("min_element", "Finding global min");
+    globptr_t minloc   = static_cast<globptr_t>(minarr[0]);
+    DASH_LOG_TRACE_VAR("min_element", minloc);
     ElementType minval = *minloc;
     for (auto i = 1; i < minarr.size(); ++i) {
+      DASH_LOG_TRACE("min_element", "Comparing local min of unit", i);
       globptr_t lmingptr = static_cast<globptr_t>(minarr[i]);
+      DASH_LOG_TRACE_VAR("min_element", lmingptr);
       // Local gptr of units might be null if unit
       // had empty range
       if (lmingptr != nullptr) {
         ElementType val = *lmingptr;
         if (compare(val, minval)) {
           minloc = lmingptr;
-          DASH_LOG_TRACE("Array.min_element", 
-                         "Setting min val to", val);
+          DASH_LOG_TRACE("min_element", "Setting current minval to", val);
           minval = val;
         }
       }
     }
+    DASH_LOG_TRACE("min_element", "Setting global min gptr to", minloc);
     min.set(minloc);
   }
+  // Wait for unit 0 to resolve global minimum
   team.barrier();
   // Minimum has been set by unit 0 at this point
   globptr_t minimum = min.get();
   if (minimum == nullptr) {
     return last;
   }
+  DASH_LOG_DEBUG("min_element >", minimum);
   return minimum;
+}
+
+/**
+ * Finds an iterator pointing to the element with the smallest value in
+ * the range [first,last).
+ * Specialization for local range, delegates to std::min_element.
+ *
+ * \return      An iterator to the first occurrence of the smallest value 
+ *              in the range, or \c last if the range is empty.
+ *
+ * \tparam      ElementType  Type of the elements in the sequence
+ * \complexity  O(d) + O(nl), with \c d dimensions in the global iterators'
+ *              pattern and \c nl local elements within the global range
+ *
+ * \ingroup     DashAlgorithms
+ */
+template< typename ElementType >
+const ElementType * min_element(
+  /// Iterator to the initial position in the sequence
+  const ElementType * first,
+  /// Iterator to the final position in the sequence
+  const ElementType * last,
+  /// Element comparison function, defaults to std::less
+  const std::function<
+      bool(const ElementType &, const ElementType)
+    > & compare
+      = std::less<const ElementType &>()) {
+  // Same as min_element with different compare function
+  return std::min_element(first, last, compare);
 }
 
 /**
@@ -121,6 +165,35 @@ GlobPtr<ElementType> max_element(
       = std::greater<const ElementType &>()) {
   // Same as min_element with different compare function
   return dash::min_element(first, last, compare);
+}
+
+/**
+ * Finds an iterator pointing to the element with the greatest value in
+ * the range [first,last).
+ * Specialization for local range, delegates to std::min_element.
+ *
+ * \return      An iterator to the first occurrence of the greatest value 
+ *              in the range, or \c last if the range is empty.
+ *
+ * \tparam      ElementType  Type of the elements in the sequence
+ * \complexity  O(d) + O(nl), with \c d dimensions in the global iterators'
+ *              pattern and \c nl local elements within the global range
+ *
+ * \ingroup     DashAlgorithms
+ */
+template< typename ElementType >
+const ElementType * max_element(
+  /// Iterator to the initial position in the sequence
+  const ElementType * first,
+  /// Iterator to the final position in the sequence
+  const ElementType * last,
+  /// Element comparison function, defaults to std::less
+  const std::function<
+      bool(const ElementType &, const ElementType)
+    > & compare
+      = std::greater<const ElementType &>()) {
+  // Same as min_element with different compare function
+  return std::min_element(first, last, compare);
 }
 
 } // namespace dash
