049d9eaa159307efb91d6f180301ffc76926c28f
Karl Fuerlinger <Karl.Fuerlinger@nm.ifi.lmu.de>
2015-12-26 09:53:56 +0100

    fixes and additions for the histo benchmark

--- dash/examples/bench.04.histo/main.cpp
+++ dash/examples/bench.04.histo/main.cpp
@@ -1,9 +1,15 @@
+#include <cstdlib>
 #include <iostream>
 #include <libdash.h>
 
 #include "../bench.h"
 
 //#define DBGOUT
+
+//#define CUMULATIVE
+
+#define COLLECTIVE_ALLOCATION
+
 
 using std::cout; 
 using std::endl;
@@ -30,21 +36,17 @@ double test_local_copy(const dash::Array<T>& keys,
 
 int main(int argc, char **argv)
 {
-  srand(31337);
-  
   dash::init(&argc, &argv);
 
-  //  perform_test<int>( 1<<8, 1<<5 );
-  //  perform_test<int>( 1<<10, 1<<7 );
-  //  perform_test<int>( 1<<12, 1<<9 );
-
-  for( int i=0; i<100; i++ ) {
-    perform_test<int>( 1<<20, 1<<16 );
-  }
-  //  perform_test<int>( 1<<19, 1<<14 );
-  //  perform_test<int>( 1<<21, 1<<15 );
-  //  perform_test<int>( 1<<23, 1<<17 );
-   
+  std::srand(31337);
+    
+  perform_test<int>( 1<<5 , 1<<3 );
+  perform_test<int>( 1<<16, 1<<11 ); // NAS class S
+  perform_test<int>( 1<<20, 1<<16 ); // NAS class W
+  perform_test<int>( 1<<23, 1<<19 ); // NAS class A
+  perform_test<int>( 1<<25, 1<<21 ); // NAS class B
+  perform_test<int>( 1<<27, 1<<23 ); // NAS class C
+      
   dash::finalize();
   
   return 0;
@@ -55,40 +57,38 @@ int main(int argc, char **argv)
 template<typename T>
 void perform_test(unsigned NUM_KEYS, unsigned MAX_KEY)
 {
-  int myid = dash::myid();
-  int size = dash::size();
+  auto myid = dash::myid();
+  auto size = dash::size();
   
   // global array of keys and histogram
   dash::Array<T> key_array(NUM_KEYS, dash::BLOCKED);
   dash::Array<T> key_histo(MAX_KEY,  dash::BLOCKED);
 
-  for( int i=0; i<key_array.lsize(); i++ ) {
+  // initialize key array to random values
+  for( auto i=0; i<key_array.lsize(); i++ ) {
     key_array.local[i]=rand() % MAX_KEY;
   }
 
-  /*
-  // initialize key array to random values
-  if(myid==0) {
-    for(int i=0; i<key_array.size(); i++ ) {
-      key_array[i]=rand() % MAX_KEY;
-    }
 #ifdef DBGOUT
+  dash::barrier();
+  if( myid==0 ) {
     cout<<"key_array:"<<endl;
-    for(int i=0; i<key_array.size(); i++ ) {
+    for(auto i=0; i<key_array.size(); i++ ) {
       cout<<(int)key_array[i]<<" ";
     }
     cout<<endl;
-#endif
   }
-
-  */
-  
+#endif
+    
+  dash::barrier();
   double t1 = test_owner_computes(key_array, key_histo);
+  dash::barrier();
   double t2 = test_local_copy(key_array, key_histo);
+  
   if(myid==0) {
-    cout<<"NUM_KEYS: "<<NUM_KEYS<<" "<<"MAX_KEYS: "<<MAX_KEY<<endl;//<<MAX_KEYS<<endl;
-    cout<<"Owner computes: MKeys/sec: "<<(NUM_KEYS*1.0e-6)/t1<<endl;
-    cout<<"Local Copy    : MKeys/sec: "<<(NUM_KEYS*1.0e-6)/t2<<endl;
+    cout<<"NUM_KEYS: "<<NUM_KEYS<<" -- "<<"MAX_KEY: "<<MAX_KEY<<endl;
+    cout<<"Owner computes : MKeys/sec: "<<(NUM_KEYS*1.0e-6)/t1<<endl;
+    cout<<"Local copy     : MKeys/sec: "<<(NUM_KEYS*1.0e-6)/t2<<endl;
     cout<<"---------------------------"<<endl;
   }
   
@@ -96,7 +96,7 @@ void perform_test(unsigned NUM_KEYS, unsigned MAX_KEY)
   dash::barrier();
   if(myid==0) {
     cout<<"key_histo:"<<endl;
-    for(int i=0; i<key_histo.size(); i++ ) {
+    for(auto i=0; i<key_histo.size(); i++ ) {
       cout<<(int)key_histo[i]<<" ";
     }
     cout<<endl;
@@ -113,17 +113,22 @@ double test_owner_computes(const dash::Array<T>& keys,
 {
   double tstart, tstop;
 
-  int myid = dash::myid();
-  int size = dash::size();
+  auto myid = dash::myid();
+  auto size = dash::size();
   
-  // TODO: use a dash::Directory instead, once its implemented...
-  dash::Array<dash::GlobPtr<int>> work_buffers(size, dash::CYCLIC);
-  work_buffers[myid] = dash::memalloc<int>(histo.size());
+  dash::Array<dash::GlobPtr<T>> work_buffers(size);
   
-  dash::GlobPtr<int> gptr = work_buffers[myid];
-  int* work_buf = (int*) gptr;
+#ifdef COLLECTIVE_ALLOCATION
+  dash::Array<T> work_histo(size*histo.size(), dash::BLOCKED);
+  work_buffers[myid] = work_histo.begin()+ myid*histo.size();
+#else
+  work_buffers[myid] = dash::memalloc<T>(histo.size());
+#endif
   
-  for(int i=0; i<histo.size(); i++) {
+  dash::GlobPtr<T> gptr = work_buffers[myid];
+  T* work_buf = (T*) gptr;
+  
+  for(auto i=0; i<histo.size(); i++) {
     work_buf[i]=0;
   }
  
@@ -131,37 +136,44 @@ double test_owner_computes(const dash::Array<T>& keys,
   TIMESTAMP(tstart);
 
   // compute the histogram for the local keys
-  for(int i=0; i<keys.lsize(); i++) {
-    work_buf[ keys.local[i] ]++; 
+  for(auto i=0; i<keys.lsize(); i++) {
+    work_buf[ keys.local[i] ]++;
   }
 
+#ifdef CUMULATIVE
   // turn it into a cumulative histogram
-  for(int i=0; i<keys.size()-1; i++ ) {
+  for(auto i=0; i<histo.size()-1; i++ ) {
     work_buf[i+1] += work_buf[i];
   }
-
+#endif
+ 
   // compute the offset of this unit's local part in 
-  // the global key_histo array
+  // the global histo array
   auto& pat = histo.pattern();
-  int goffs = pat.global(0);
+  auto goffs = pat.global(0);
 
   // initialize my part of the global histogram with my own contribution
-  for(int i=0; i<histo.lsize(); i++ ) { 
+  for(auto i=0; i<histo.lsize(); i++ ) { 
     histo.local[i] = work_buf[goffs+i];
   }
 
-  for(int unit=1; unit<size; unit++ ) {
-    dash::GlobPtr<int> remote = work_buffers[(myid+unit)%size];
+  dash::barrier();
+  
+  for(auto unit=1; unit<size; unit++ ) {
+    dash::GlobPtr<T> remote = work_buffers[(myid+unit)%size];
     
-    for(int i=0; i<histo.lsize(); i++ ) { 
+    for(auto i=0; i<histo.lsize(); i++ ) { 
       histo.local[i] += remote[goffs+i];
     }
   }
+  
   dash::barrier();
   TIMESTAMP(tstop);
 
-  dash::GlobPtr<int> gp=work_buffers[myid];
-  dash::memfree<int>(gp);
+#ifndef COLLECTIVE_ALLOCATION
+  dash::GlobPtr<T> gp=work_buffers[myid];
+  dash::memfree<T>(gp);
+#endif
   
   return (tstop-tstart);
 }
@@ -172,17 +184,22 @@ double test_local_copy(const dash::Array<T>& keys,
 {
   double tstart, tstop;
 
-  int myid = dash::myid();
-  int size = dash::size();
+  auto myid = dash::myid();
+  auto size = dash::size();
   
-  // use a dash::Directory instead once its implemented...
-  dash::Array<dash::GlobPtr<int>> work_buffers(size, dash::CYCLIC);
-  work_buffers[myid] = dash::memalloc<int>(histo.size());
+  dash::Array<dash::GlobPtr<T>> work_buffers(size);
   
-  dash::GlobPtr<int> gptr = work_buffers[myid];
-  int* work_buf = (int*) gptr;
-
-  for(int i=0; i<histo.size(); i++) {
+#ifdef COLLECTIVE_ALLOCATION
+  dash::Array<T> work_histo(size*histo.size(), dash::BLOCKED);
+  work_buffers[myid] = work_histo.begin()+ myid*histo.size();
+#else
+  work_buffers[myid] = dash::memalloc<T>(histo.size());
+#endif
+  
+  dash::GlobPtr<T> gptr = work_buffers[myid];
+  T* work_buf = (T*) gptr;
+  
+  for(auto i=0; i<histo.size(); i++) {
     work_buf[i]=0;
   }
  
@@ -190,37 +207,48 @@ double test_local_copy(const dash::Array<T>& keys,
   TIMESTAMP(tstart);
 
   // compute the histogram for the local keys
-  for(int i=0; i<keys.lsize(); i++) {
-    work_buf[ keys.local[i] ]++; 
+  for(auto i=0; i<keys.lsize(); i++) {
+    work_buf[ keys.local[i] ]++;
   }
 
+#ifdef CUMULATIVE
   // turn it into a cumulative histogram
-  for(int i=0; i<keys.size()-1; i++ ) {
+  for(auto i=0; i<histo.size()-1; i++ ) {
     work_buf[i+1] += work_buf[i];
   }
-
+#endif
+ 
   // compute the offset of this unit's local part in 
-  // the global key_histo array
+  // the global histo array
   auto& pat = histo.pattern();
-  int goffs = pat.global(0);
+  auto goffs = pat.global(0);
 
   // initialize my part of the global histogram with my own contribution
-  for(int i=0; i<histo.lsize(); i++ ) { 
+  for(auto i=0; i<histo.lsize(); i++ ) { 
     histo.local[i] = work_buf[goffs+i];
   }
 
-  for(int unit=1; unit<size; unit++ ) {
-    dash::GlobPtr<int> remote = work_buffers[(myid+unit)%size];
-    dash::LocalCopy<int> lc(remote, histo.lsize());
+  dash::barrier();
+  
+  for(auto unit=1; unit<size; unit++ ) {
+    dash::GlobPtr<T> remote = work_buffers[(myid+unit)%size];
+
+    dash::LocalCopy<T> lc(remote+goffs, histo.lsize());
     lc.get();
     
-    for(int i=0; i<histo.lsize(); i++ ) { 
+    for(auto i=0; i<histo.lsize(); i++ ) { 
       histo.local[i] += lc[i];
     }
   }
+  
   dash::barrier();
   TIMESTAMP(tstop);
 
+#ifndef COLLECTIVE_ALLOCATION
+  dash::GlobPtr<T> gp=work_buffers[myid];
+  dash::memfree<T>(gp);
+#endif
+  
   return (tstop-tstart);
 }
 
