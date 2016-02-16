eec177382d9973f7324ec2a2a4e0ed49d5a6907d
Tobias Fuchs <fuchst@nm.ifi.lmu.de>
2016-02-16 20:57:29 +0100

    Extented pattern visualizer example

--- dash/examples/ex.06.pattern-visualizer/main.cpp
+++ dash/examples/ex.06.pattern-visualizer/main.cpp
@@ -29,31 +29,36 @@ int main(int argc, char* argv[])
 
   if (dash::myid() == 0)
   {
-    TeamSpec<2> ts(2,2);
+    TeamSpec<2> ts2d(2,2);
+    TeamSpec<2> ts1d(2,1);
 
-    print_example(TilePattern<2>(20,15, TILE(1),TILE(5), ts),
-      "TilePattern_tile-1x5_rowmaj.svg",
+    print_example(TilePattern<2>(20,15, TILE(1),TILE(5), ts2d),
+      "TilePattern_tile-1x5_team-2x2_rowmaj.svg",
       "TilePattern<2>(20,15, TILE(1), TILE(5), TeamSpec<2>(2,2))");
 
-    print_example(TilePattern<2>(20,15, TILE(2),TILE(5), ts),
-      "TilePattern_tile-2x5_rowmaj.svg",
+    print_example(TilePattern<2>(20,15, TILE(2),TILE(5), ts2d),
+      "TilePattern_tile-2x5_team-2x2_rowmaj.svg",
       "TilePattern<2>(20,15, TILE(2), TILE(5), TeamSpec<2>(2,2))");
 
-    print_example(TilePattern<2, COL_MAJOR>(20,15, TILE(5),TILE(5), ts),
-      "TilePattern_tile-5x5_colmaj.svg",
+    print_example(TilePattern<2, COL_MAJOR>(20,15, TILE(5),TILE(5), ts2d),
+      "TilePattern_tile-5x5_team-2x2_colmaj.svg",
       "TilePattern<2, COL_MAJOR>(20,15, TILE(5),TILE(5), TeamSpec<2>(2,2))");
 
-    print_example(TilePattern<2>(20,15, TILE(5),TILE(1), ts),
-      "TilePattern_tile-5x1_team-2x2.svg",
+    print_example(TilePattern<2>(20,15, TILE(5),TILE(1), ts2d),
+      "TilePattern_tile-5x1_team-2x2_rowmajor.svg",
       "TilePattern<2>(20,15, TILE(5), TILE(1), TeamSpec<2>(2,2))");
 
-    print_example(Pattern<2>(20,15, BLOCKED,CYCLIC, ts),
-      "BlockPattern_blocked-cyclic_rowmaj.svg",
+    print_example(Pattern<2>(20,15, BLOCKED,CYCLIC, ts2d),
+      "BlockPattern_blocked-cyclic_team-2x2_rowmaj.svg",
       "Pattern<2>(20,15, BLOCKED,CYCLIC, TeamSpec<2>(2,2))");
 
-    print_example(ShiftTilePattern<2>(32,24, TILE(4), TILE(3)),
-      "ShiftTilePattern_4x5_rowmaj.svg",
+    print_example(ShiftTilePattern<2>(32,24, TILE(4), TILE(3), ts2d),
+      "ShiftTilePattern_4x5_team-2x2_rowmaj.svg",
       "ShiftTilePattern<2>(32,24, TILE(4),TILE(5))");
+
+    print_example(ShiftTilePattern<2>(20,20, TILE(1), TILE(5), ts1d),
+      "ShiftTilePattern_4x5_team-2x1_rowmaj.svg",
+      "ShiftTilePattern<2>(20,20, TILE(1),TILE(5), TeamSpec<2>(2,1))");
   }
 
   dash::finalize();
