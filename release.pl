dd04e03ad6fd65983eae401d09801ca73bfc8915
Karl Fuerlinger <Karl.Fuerlinger@nm.ifi.lmu.de>
2016-03-01 14:33:30 +0100

    Added a simple release script for a tarball release

---
+++ release.pl
@@ -0,0 +1,105 @@
+#!/usr/bin/perl
+
+use File::Basename;
+use File::Slurp;
+
+$rdir    = "RELEASE";
+$version = "0.2.0";
+$base    = "$rdir/dash-$version";
+
+if( -e "$base" ) {
+    print "Directory $base exists, exiting.\n";
+    exit;
+}
+
+$license = read_file("LICENSE");
+
+@files = ("LICENSE",
+	  "README.md",
+	  "CHANGELOG.md",
+	  "CMakeLists.txt",
+	  "CMakeExt/*.cmake",
+	  "build.sh",
+	  "config/*.cmake",
+	  #
+	  # DART interface specification
+	  #
+	  "dart-if/v3.2/include/dash/dart/if/*.h",
+	  "dart-if/CMakeLists.txt",
+	  #
+	  # DART base implementation
+	  #
+	  "dart-impl/base/include/dash/dart/base/*.h",
+	  "dart-impl/base/src/*.c",
+	  "dart-impl/base/CMakeLists.txt",
+	  #
+	  # DART MPI implementation
+	  #
+	  "dart-impl/mpi/include/dash/dart/mpi/*.h",
+	  "dart-impl/mpi/src/*.c",
+	  "dart-impl/mpi/src/Makefile",
+	  "dart-impl/mpi/make.defs",
+	  "dart-impl/mpi/CMakeLists.txt",
+	  #
+	  # DASH source and header files
+	  #
+	  "dash/include/*.h",
+	  "dash/include/dash/*.h",
+	  "dash/include/dash/algorithm/*.h",
+	  "dash/include/dash/bindings/*.h",
+	  "dash/include/dash/exception/*.h",
+	  "dash/include/dash/matrix/*.h",
+	  "dash/include/dash/matrix/internal/*.h",
+	  "dash/include/dash/tools/*.h",
+	  "dash/include/dash/internal/*.h",
+	  "dash/include/dash/util/*.h",
+	  "dash/include/dash/util/internal/*.h",
+	  "dash/src/*.cc",
+	  "dash/src/util/*.cc",
+	  "dash/src/algorithm/*.cc",
+	  "dash/src/Makefile",
+	  "dash/make.defs",
+	  "dash/CMakeLists.txt",
+	  #
+	  # DASH examples
+	  #
+	  "dash/examples/Makefile_cpp",
+	  "dash/examples/ex*/*.cpp",
+	  "dash/examples/ex*/Makefile",
+	  "dash/examples/Makefile",
+	  "dash/examples/CMakeLists.txt",
+    );
+
+
+foreach $dir (@files)
+{
+    foreach $file (glob($dir))
+    {
+	$dirname  = dirname("$base/$file");
+
+	system("mkdir -p $dirname");
+#
+#	open(IN , "$file") or die("Could not open '$file'.");
+#	open(OUT, ">$base/$file") or die("Could not open '$base/$file'.");
+#	foreach $line (<IN>) {
+#	    if( $line=~/\@DASH_HEADER\@/ ) {
+#		print OUT "$license";
+#	    } else {
+#		print OUT "$line";
+#	    }
+#	}
+#	close(IN);
+#	close(OUT);
+
+	print "copying '$file'\n";
+	system("cp $file $dirname");
+
+    }
+}
+
+
+#foreach $dir (@directories)
+#{
+#    system("mkdir -p $rdir/dash-$version/$dir");
+#}
+#print dirname("aasdf/asdfasdf*.pl");
