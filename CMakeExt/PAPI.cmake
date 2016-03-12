a0458741e807e163e741ddbdb9a4de7b8455c103
Tobias Fuchs <fuchst@nm.ifi.lmu.de>
2016-03-12 23:25:40 +0100

    Reverted linking to static libraries in build

--- CMakeExt/PAPI.cmake
+++ CMakeExt/PAPI.cmake
@@ -31,7 +31,7 @@ message(STATUS "Searching for PAPI in path " ${PAPI_PREFIX})
 find_library(
   PAPI_LIBRARIES
   # Pick the static library first for easier run-time linking.
-  NAMES libpapi.a papi
+  NAMES papi
   HINTS ${PAPI_PREFIX}/lib
 )
 
