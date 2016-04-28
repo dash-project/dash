94e974a484f7ff1eb1db781c4f6a0b80369a8e1a
Felix Moessbauer <felix.moessbauer@campus.lmu.de>
2016-04-28 16:52:33 +0200

    Designated dash::fill as a collective operation in the documentation

--- dash/include/dash/algorithm/Fill.h
+++ dash/include/dash/algorithm/Fill.h
@@ -11,6 +11,9 @@ namespace dash {
 /**
  * Assigns the given value to the elements in the range [first, last)
  *
+ * Being a collaborative operation, each unit will assign the value to
+ * its local elements only.
+ *
  * \tparam      ElementType  Type of the elements in the sequence
  * \complexity  O(d) + O(nl), with \c d dimensions in the global iterators'
  *              pattern and \c nl local elements within the global range
@@ -18,21 +21,21 @@ namespace dash {
  * \ingroup     DashAlgorithms
  */
 template<
-  typename ElementType,
-  class PatternType>
+    typename ElementType,
+    class PatternType>
 void fill(
-  /// Iterator to the initial position in the sequence
-  GlobIter<ElementType, PatternType>  first,
-  /// Iterator to the final position in the sequence
-  GlobIter<ElementType, PatternType>  last,
-  /// Value which will be assigned to the elements in range [first, last)
-  const ElementType & value) {
-  /// Global iterators to local range:
-  auto index_range  = dash::local_range(first, last);
-  auto lfirst = index_range.begin;
-  auto llast = index_range.end;
+    /// Iterator to the initial position in the sequence
+    GlobIter<ElementType, PatternType>  first,
+    /// Iterator to the final position in the sequence
+    GlobIter<ElementType, PatternType>  last,
+    /// Value which will be assigned to the elements in range [first, last)
+    const ElementType & value) {
+    /// Global iterators to local range:
+    auto index_range  = dash::local_range(first, last);
+    auto lfirst = index_range.begin;
+    auto llast = index_range.end;
 
-  std::fill(lfirst, llast, value);
+    std::fill(lfirst, llast, value);
 }
 
 } // namespace dash
