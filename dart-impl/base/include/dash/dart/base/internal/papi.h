01302d3ed9c6462beef505a1f1b58bbae29841eb
Tobias Fuchs <fuchst@nm.ifi.lmu.de>
2016-05-01 07:59:30 +0200

    dart-base: Fixed compiler errors

--- dart-impl/base/include/dash/dart/base/internal/papi.h
+++ dart-impl/base/include/dash/dart/base/internal/papi.h
@@ -7,13 +7,16 @@
 #define DART__BASE__INTERNAL__PAPI_H__
 
 #ifdef DART_ENABLE_PAPI
+
+#include <dash/dart/if/dart_types.h>
 #include <papi.h>
 
-static void dart__base__locality__papi_handle_error(
-  int papi_ret)
 
-static dart_ret_t dart__base__locality__papi_init(
-  const PAPI_hw_info_t ** hwinfo)
+void dart__base__locality__papi_handle_error(
+  int papi_ret);
+
+dart_ret_t dart__base__locality__papi_init(
+  const PAPI_hw_info_t ** hwinfo);
 
 #endif /* DART_ENABLE_PAPI */
 
