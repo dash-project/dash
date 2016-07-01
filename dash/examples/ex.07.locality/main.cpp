b60b09f65d055c051fc53e70816bed2c1a88a60a
Tobias Fuchs <tobias.fuchs@nm.ifi.lmu.de>
2016-07-01 19:17:04 +0200

    Added basic example illustrating unit placement

--- dash/examples/ex.07.locality/main.cpp
+++ dash/examples/ex.07.locality/main.cpp
@@ -18,27 +18,6 @@ int main(int argc, char * argv[])
 
   pid_t pid;
   char buf[100];
-  bool locality_split   = false;
-  int  split_num_groups = 3;
-  dart_locality_scope_t split_scope = DART_LOCALITY_SCOPE_NODE;
-
-  if (argc >= 2) {
-    if (std::string(argv[1]) == "-s") {
-      locality_split   = false;
-      split_num_groups = static_cast<int>(strtol(argv[2], NULL, 10));
-    } else if (std::string(argv[1]) == "-ls") {
-      locality_split   = true;
-      split_scope      = DART_LOCALITY_SCOPE_NODE;
-      if (std::string(argv[2]) == "module") {
-        split_scope = DART_LOCALITY_SCOPE_MODULE;
-      } else if (std::string(argv[2]) == "numa") {
-        split_scope = DART_LOCALITY_SCOPE_NUMA;
-      }
-      if (argc >= 4) {
-        split_num_groups = static_cast<int>(strtol(argv[3], NULL, 10));
-      }
-    }
-  }
 
   dash::init(&argc, &argv);
 
@@ -47,7 +26,7 @@ int main(int argc, char * argv[])
   bench_params.print_pinning();
 
   dart_barrier(DART_TEAM_ALL);
-  sleep(5);
+  sleep(3);
 
   auto myid = dash::myid();
   auto size = dash::size();
@@ -57,26 +36,6 @@ int main(int argc, char * argv[])
 
   std::string separator(80, '=');
 
-  dart_barrier(DART_TEAM_ALL);
-  sleep(2);
-  if (myid == 0) {
-    cout << "Usage:" << endl
-         << "  ex.07.locality [-s <num_split_groups> | -ls <split_scope>]"
-         << endl
-         << endl
-         << "  ex.07.locality ";
-    if (locality_split) {
-      cout << "-ls " << argv[2] << " " << split_num_groups << ": "
-           << "locality split into " << split_num_groups << " groups "
-           << "at scope " << split_scope << endl;
-    } else {
-      cout << "-s " << split_num_groups << ": "
-           << "regular split into " << split_num_groups << " groups" << endl;
-    }
-    cout << separator << endl;
-  } else {
-    sleep(2);
-  }
   dart_barrier(DART_TEAM_ALL);
   sleep(1);
 
@@ -89,7 +48,7 @@ int main(int argc, char * argv[])
   cout << i_os.str();
 
   dart_barrier(DART_TEAM_ALL);
-  sleep(5);
+  sleep(2);
 
   if (myid == 0) {
     cout << separator << endl;
@@ -102,42 +61,6 @@ int main(int argc, char * argv[])
          << separator
          << endl;
   } else {
-    sleep(2);
-  }
-
-  auto & split_team = locality_split
-                      ? dash::Team::All().locality_split(split_scope,
-                                                         split_num_groups)
-                      : dash::Team::All().split(split_num_groups);
-
-  std::ostringstream t_os;
-  t_os << "Unit id " << setw(3) << myid << " -> "
-       << "unit id " << setw(3) << split_team.myid() << " "
-       << "in team " << split_team.dart_id() << " after split"
-       << endl;
-  cout << t_os.str();
-
-  dart_barrier(DART_TEAM_ALL);
-  sleep(2);
-
-  for (int g = 0; g < split_num_groups; ++g) {
-    if (split_team.dart_id() == 1+g && split_team.myid() == 0) {
-      cout << "Locality domains of unit 0 "
-           << "in team " << split_team.dart_id() << ":" << endl
-           << endl;
-
-      dart_domain_locality_t * global_domain_locality;
-      dart_domain_team_locality(
-        split_team.dart_id(), ".", &global_domain_locality);
-
-      cout << *global_domain_locality
-           << endl
-           << separator
-           << endl;
-    } else {
-      sleep(2);
-    }
-    dart_barrier(DART_TEAM_ALL);
     sleep(2);
   }
 
