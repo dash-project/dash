0497e366e71e935947bf655b04df366522d399fb
Felix Moessbauer <felix.moessbauer@gmail.com>
2016-07-04 15:35:16 +0200

    Mark tests internally as failed on all units if one unit fails

--- dash/test/TestPrinterTest.cc
+++ dash/test/TestPrinterTest.cc
@@ -19,12 +19,3 @@ TEST_F(TestPrinterTest, FailOnOneUnit) {
   SUCCEED();
 #endif
 }
-
-TEST_F(TestPrinterTest, FailWithExpect) {
-// To test the TestPrinter, enable this test and run with at least 2 units
-#if 0
-  EXPECT_EQ(1,2);
-#else
-  SUCCEED();
-#endif
-}
