4124ded592007b60f8024427117ec9467f47ee59
Josef Schaeffer <josef.schaeffer@me.com>
2016-07-20 12:14:11 +0200

    New algorithms implemented

---
+++ dash/include/dash/algorithm/Any_of.h
@@ -0,0 +1,30 @@
+#ifndef DASH__ALGORITHM__ANY_OF_H__
+#define DASH__ALGORITHM__ANY_OF_H__
+
+#include <dash/iterator/GlobIter.h>
+#include <dash/algorithm/LocalRange.h>
+#include <dash/algorithm/Operation.h>
+#include <dash/dart/if/dart_communication.h>
+
+namespace dash {
+
+/**
+ * \ingroup     DashAlgorithms
+ */
+template<
+    typename ElementType,
+    class PatternType>
+GlobIter<ElementType, PatternType> any_of(
+    /// Iterator to the initial position in the sequence
+    GlobIter<ElementType, PatternType>   first,
+    /// Iterator to the final position in the sequence
+    GlobIter<ElementType, PatternType>   last,
+    /// Predicate applied to the elements in range [first, last)
+    UnaryPredicate p)
+{
+    
+    return find_if(first, last, p) != last;
+
+} // namespace dash
+
+#endif // DASH__ALGORITHM__ANY_OF_H__
