94afe032d16de56fcabc01380a93023e16162462
Felix Moessbauer <felix.moessbauer@cip.ifi.lmu.de>
2016-07-25 18:40:50 +0200

    added missig GenerateConfig file

--- CMakeExt/GenerateConfig.cmake
+++ CMakeExt/GenerateConfig.cmake
@@ -2,15 +2,8 @@
 
 # all dash modules
 set(DASH_MODULES PAPI HWLOC LIKWID NUMA PLASMA HDF5 MKL BLAS LAPACK SCALAPACK)
-# dash algorithms
-set(DASH_ALGORITHMS SUMMA)
 
 # set all variables to false as default
 foreach(DASH_MODULE ${DASH_MODULES})
   set(CONF_AVAIL_${DASH_MODULE} "false")
-endforeach()
-
-foreach(DASH_ALGO ${DASH_ALGORITHMS})
-  message("ALGO: ${DASH_ALGO}")
-  set(CONF_AVAIL_ALGO_${DASH_ALGO} "false")
 endforeach()
