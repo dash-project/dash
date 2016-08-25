449fe8b664d1a86e56726d288c61479ea737c06d
Josef <josef.schaeffer@me.com>
2016-08-25 13:27:06 +0000

    Fixed compiler errors in dash/test/FindTest.*

--- dash/test/FindTest.h
+++ dash/test/FindTest.h
@@ -8,17 +8,13 @@
 /**
  * Test fixture for algorithm dash::find.
  */
-class FindTest : public ::testing::Test {
+class FindTest : public ::testing::Test
+{
 protected:
-<<<<<<< HEAD
-  typedef int                                        Element_t;
-  typedef dash::Array<Element_t>                     Array_t;
-  typedef typename Array_t::pattern_type::index_type index_t;
-=======
+
   typedef int                                         Element_t;
   typedef dash::Array<Element_t>                      Array_t;
   typedef typename Array_t::pattern_type::index_type  index_t;
->>>>>>> ebf1c0aa9589c55aa4c46194e2ff44d41f5e4650
 
   size_t _num_elem = 251;
 
