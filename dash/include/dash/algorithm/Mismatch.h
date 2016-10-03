1baf7ded91bbf55cbd390d6a5e02159eef5ccbdf
Tobias Fuchs <fuchst@nm.ifi.lmu.de>
2016-10-03 23:07:44 +0200

    Fixing dash::mismatch

--- dash/include/dash/algorithm/Mismatch.h
+++ dash/include/dash/algorithm/Mismatch.h
@@ -10,64 +10,66 @@
 #include <utility>
 #include <algorithm>
 
+
 namespace dash {
 
 /**
  * \ingroup DashAlgorithms
  */
-
 template<
- typename ElementType,
- class    PatternType,
- class    BinaryPredicate >
+  typename ElementType,
+  class    PatternType,
+  class    BinaryPredicate >
 std::pair<GlobIter<ElementType, PatternType>,
           GlobIter<ElementType, PatternType>>
 mismatch(
- GlobIter<ElementType, PatternType> input_1_f,
- GlobIter<ElementType, PatternType> input_1_l,
- GlobIter<ElementType, PatternType> input_2_f,
- GlobIter<ElementType, PatternType> input_2_l,
- BinaryPredicate p)
+  GlobIter<ElementType, PatternType> input_1_f,
+  GlobIter<ElementType, PatternType> input_1_l,
+  GlobIter<ElementType, PatternType> input_2_f,
+  GlobIter<ElementType, PatternType> input_2_l,
+  BinaryPredicate p)
 {
- typedef default_index_t index_t;
+  typedef default_index_t                                index_t;
+  typedef std::pair<GlobIter<ElementType, PatternType>,
+                    GlobIter<ElementType, PatternType>>  result_t;
 
- auto myid      = dash::myid();
+  auto myid      = dash::myid();
 
- // Lokale Iterator-Bereiche anlegen
- auto index_1   = dash::local_range(input_1_f, input_1_l);
- auto index_2   = dash::local_range(input_2_f, input_2_l);
+  // Lokale Iterator-Bereiche anlegen
+  auto index_1   = dash::local_range(input_1_f, input_1_l);
+  auto index_2   = dash::local_range(input_2_f, input_2_l);
 
- // Ueber einem lokalen Iterator-Bereich nach mismatch suchen mit std::
- auto l_result  = std::mismatch(index_1.begin, index_1.end,
-                                index_2.begin,
-                                p);
+  // Ueber einem lokalen Iterator-Bereich nach mismatch suchen mit std::
+  auto l_result  = std::mismatch(index_1.begin, index_1.end,
+                                 index_2.begin,
+                                 p);
 
- // Lokale Suchergebnisse auf globalem Array speichern
- dash::Array<decltype(l_result)> l_results(dash::size());
- l_results.local[0] = l_result;
+  // Lokale Suchergebnisse auf globalem Array speichern
+  dash::Array<decltype(l_result)> l_results(dash::size());
+  l_results.local[0] = l_result;
 
- l_results.barrier();
+  l_results.barrier();
 
- //Globale Referenz anlegen, ob tatsaechlich mismatch vorliegt
- dash::Array<index_t> g_reference(dash::size());
+  // Globale Referenz anlegen, ob tatsaechlich mismatch vorliegt
+  dash::Array<index_t> g_reference(dash::size());
 
- if (l_result.first == index_1.end) {
-   g_reference.local[0] = -1;
- } else {
-   g_reference.local[0] = 0;
- }
- g_reference.barrier();
+  if (l_result.first == index_1.end) {
+    g_reference.local[0] = -1;
+  } else {
+    g_reference.local[0] = 0;
+  }
+  g_reference.barrier();
 
- index_t no_mismatch  = -1;
+  index_t no_mismatch  = -1;
 
- for (index_t u = 0; u < dash::size(); u++) {
-   auto ref = g_reference[u];
-   if (ref != no_mismatch) {
-     auto ret = l_results[u];
-     return ret;
-   }
- }
- return std::make_pair(input_1_l, input_2_l);
+  for (index_t u = 0; u < dash::size(); u++) {
+    auto ref = g_reference[u];
+    if (ref != no_mismatch) {
+      result_t ret = l_results[u];
+      return ret;
+    }
+  }
+  return std::make_pair(input_1_l, input_2_l);
 }
 
 } // namespace dash
