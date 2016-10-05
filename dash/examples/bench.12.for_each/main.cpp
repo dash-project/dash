e684bc280f0080c6e1ddb37c3918ebafff2a52b2
Felix Moessbauer <felix.moessbauer@campus.lmu.de>
2016-10-05 19:50:28 +0200

    added for_each comparison benchmark

---
+++ dash/examples/bench.12.for_each/main.cpp
@@ -0,0 +1,222 @@
+/**
+ * Measures the performance of different
+ * for_each implementations on dash containers
+ */
+
+#include <libdash.h>
+#include <iostream>
+#include <iomanip>
+#include <string>
+
+using std::cout;
+using std::endl;
+using std::setw;
+using std::setprecision;
+
+typedef dash::util::Timer<
+          dash::util::TimeMeasure::Clock
+        > Timer;
+
+typedef typename dash::util::BenchmarkParams::config_params_type
+  bench_cfg_params;
+
+typedef struct benchmark_params_t {
+  long   size_base;
+  int    max_time;
+} benchmark_params;
+
+typedef struct measurement_t {
+  std::string testcase;
+  double      local_elems_ns;
+  double      local_size_mb;
+  double      time_fill_s;
+  double      time_foreach_s;
+  double      time_total_s;
+} measurement;
+
+void print_measurement_header();
+void print_measurement_record(
+  const bench_cfg_params & cfg_params,
+  measurement              measurement,
+  const benchmark_params & params);
+
+benchmark_params parse_args(int argc, char * argv[]);
+
+void print_params(
+  const dash::util::BenchmarkParams & bench_cfg,
+  const benchmark_params            & params);
+
+
+measurement evaluate(
+              long size,
+              std::string testcase,
+              benchmark_params params);
+
+int main(int argc, char** argv)
+{
+  dash::init(&argc, &argv);
+
+  // 0: real, 1: virt
+  Timer::Calibrate(0);
+
+  measurement res;
+
+  dash::util::BenchmarkParams bench_params("bench.12.for_each");
+  bench_params.print_header();
+  bench_params.print_pinning();
+
+  benchmark_params params = parse_args(argc, argv);
+  auto bench_cfg = bench_params.config();
+
+  print_params(bench_params, params);
+  print_measurement_header();
+
+  int     multiplier = 1;
+  double  round_time = 0;
+  std::array<std::string, 4> testcases {{
+                            "std::for_each l.",
+                            "dash::for_each",
+                            "dash::stl_for_each",
+                            "dash::for_each_with_index" }};
+
+  while(round_time < params.max_time){
+    auto    time_start = Timer::Now();
+    for(auto testcase : testcases){
+      res = evaluate(params.size_base*multiplier, testcase, params);
+      print_measurement_record(bench_cfg, res, params);
+    }
+    multiplier*=2;
+    round_time = Timer::ElapsedSince(time_start) / (1000 * 1000);
+  }
+
+//  res = evaluate(params.size_base, "dash::stl_for_each", params);
+//  print_measurement_record(bench_cfg, res, params);
+
+
+  if( dash::myid()==0 ) {
+    cout << "Benchmark finished" << endl;
+  }
+
+  dash::finalize();
+  return 0;
+}
+
+measurement evaluate(long size, std::string testcase, benchmark_params params)
+{
+  measurement mes;
+  long sum = 0;
+  dash::NArray<int, 2> container(size, size);
+  auto begin  = container.begin();
+  auto lbegin = container.lbegin();
+  auto end    = container.end();
+  auto lend   = container.lend();
+  auto lsize  = container.local.size();
+ 
+  // inefficient sum
+  std::function<void(const int &)> stl_for_each =
+      [&sum](int el){
+        sum+=el;
+      };
+  std::function<void(const int &, long)> for_each_index =
+    [&sum](int el, long idx) {
+      sum+=el;
+    };
+  std::function<void(long)> for_each =
+    [&sum, &container](long idx) {
+      sum+= *(container.begin() + idx);
+    };
+
+
+  dash::barrier();
+  auto ts_tot_start = Timer::Now();
+  dash::fill(begin, end, 1);
+  mes.time_fill_s = Timer::ElapsedSince(ts_tot_start) / (1000 * 1000);
+
+  auto ts_foreach_start = Timer::Now();
+
+  if(testcase == "dash::stl_for_each"){
+    dash::stl_for_each(begin, end, stl_for_each);
+  } else if(testcase == "dash::for_each"){
+    dash::for_each(begin, end, for_each);
+  } else if(testcase == "dash::for_each_with_index"){
+    dash::for_each_with_index(begin, end, for_each_index);
+  } else if(testcase == "std::for_each l."){
+    std::for_each(lbegin, lend, stl_for_each);
+  }
+
+  mes.time_foreach_s = Timer::ElapsedSince(ts_foreach_start) / (1000 * 1000);
+  mes.time_total_s   = Timer::ElapsedSince(ts_tot_start) / (1000 * 1000);
+  mes.local_elems_ns = lsize / mes.time_foreach_s;
+  mes.local_size_mb  = static_cast<double>((lsize * sizeof(int))/(1024*1024));
+  mes.testcase       = testcase;
+  return mes;
+}
+
+void print_measurement_header()
+{
+  if (dash::myid() == 0) {
+    cout << std::right
+         << std::setw(5)  << "units"          << ","
+         << std::setw(9)  << "mpi.impl"       << ","
+         << std::setw(12) << "l. size mb"     << ","
+         << std::setw(13) << "l. elems.ns"    << ","
+         << std::setw(25) << "impl."          << ","
+         << std::setw(12) << "time fill.s"    << ","
+         << std::setw(14) << "time foreach.s" << ","
+         << std::setw(12) << "time total.s" 
+         << endl;
+  }
+}
+
+void print_measurement_record(
+  const bench_cfg_params & cfg_params,
+  measurement              measurement,
+  const benchmark_params & params)
+{
+  if (dash::myid() == 0) {
+    std::string mpi_impl = dash__toxstr(MPI_IMPL_ID);
+    auto mes = measurement;
+        cout << std::right
+         << std::setw(5) << dash::size() << ","
+         << std::setw(9) << mpi_impl     << ","
+         << std::fixed << setprecision(2) << setw(12) << mes.local_size_mb  << ","
+         << std::fixed << setprecision(2) << setw(12) << (mes.local_elems_ns / 1000) << "k,"
+         << std::fixed << setprecision(2) << setw(25) << mes.testcase       << ","
+         << std::fixed << setprecision(2) << setw(12) << mes.time_fill_s    << ","
+         << std::fixed << setprecision(2) << setw(14) << mes.time_foreach_s << ","
+         << std::fixed << setprecision(2) << setw(12) << mes.time_total_s
+         << endl;
+  }
+}
+
+benchmark_params parse_args(int argc, char * argv[])
+{
+  benchmark_params params;
+  params.size_base      = 1000;
+  params.max_time       = 20;
+
+  for (auto i = 1; i < argc; i += 2) {
+    std::string flag = argv[i];
+    if (flag == "-sb") {
+      params.size_base      = atoi(argv[i+1]);
+    }
+    if (flag == "-tmax") {
+      params.size_base      = atoi(argv[i+1]);
+    }
+  }
+  return params;
+}
+
+void print_params(
+  const dash::util::BenchmarkParams & bench_cfg,
+  const benchmark_params            & params)
+{
+  if (dash::myid() != 0) {
+    return;
+  }
+
+  bench_cfg.print_section_start("Runtime arguments");
+  bench_cfg.print_param("-sb",    "initial matrix size", params.size_base);
+  bench_cfg.print_param("-tmax",  "max time in s per iteration", params.size_base);
+  bench_cfg.print_section_end();
+}
