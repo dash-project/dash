63a14cc0ecedced1f9cc540a70f4de430c1ce805
Clemens Manert <Clemens@Manert.de>
2016-10-09 04:06:21 +0200

    Added test with specified operation. (String concatination)

--- dash/test/AccumulateTest.cc
+++ dash/test/AccumulateTest.cc
@@ -24,3 +24,30 @@ TEST_F(AccumulateTest, SimpleConstructor) {
     ASSERT_EQ_U(num_elem_total * value, result);
   }
 }
+
+TEST_F(AccumulateTest, StringConcatOperaton) {
+  const size_t num_elem_local = 100;
+  size_t num_elem_total       = _dash_size * num_elem_local;
+  auto value = 2;
+  
+  // Create a vector
+  dash::Array<int> target(4);
+  target[0] = 1;
+  target[1] = 2;
+  target[2] = 3;
+  target[3] = 4;
+  
+  dash::barrier();
+
+  std::string result = std::accumulate(
+				       std::next(target.begin()),
+				       target.end(),
+				       std::to_string(target[0]), // start element
+                                    [](std::string x1, int x2) {
+                                        return x1 + '-' + std::to_string(x2);
+                                    });
+
+  if(dash::myid() == 0) {
+    ASSERT_STREQ("1-2-3-4", result.c_str());
+  }
+}
