fc01320f8495e573c90e726b3d76229e73d2cc72
Clemens Manert <Clemens@Manert.de>
2016-10-14 15:49:24 +0200

    5: Added testcases for initializer list constructor.

--- dash/test/GlobMemTest.cc
+++ dash/test/GlobMemTest.cc
@@ -3,3 +3,30 @@
 #include "TestBase.h"
 #include "GlobMemTest.h"
 
+TEST_F(GlobMemTest, ConstructorInitializer_list)
+{
+  DASH_LOG_TRACE("GlobMemTestxxx >",
+		 " Constructor");
+
+  ASSERT_EQ_U(7, 2);
+
+  auto target_local_elements = {1,2,3,4,5,6};
+  auto target = dash::GlobMem<int>(target_local_elements);
+  
+  int target_element;
+  target.get_value(&target_element, 0);
+  ASSERT_EQ_U(7, target_element);
+
+  target.get_value(&target_element, 1);
+  ASSERT_EQ_U(2, target_element);
+
+  target.get_value(&target_element, 2);
+  ASSERT_EQ_U(3, target_element);
+
+  target.get_value(&target_element, 3);
+  ASSERT_EQ_U(4, target_element);
+
+  target.get_value(&target_element, 4);
+  ASSERT_EQ_U(5, target_element);
+}
+
