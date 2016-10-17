9dca56afdb450d816694355b3e0aaf075ff9aaa0
Felix Moessbauer <felix.moessbauer@gmail.com>
2016-10-17 10:32:51 +0200

    do not pass references in contructor of patterns

--- dash/include/dash/pattern/BlockPattern1D.h
+++ dash/include/dash/pattern/BlockPattern1D.h
@@ -224,14 +224,14 @@ public:
    */
   Pattern(
     /// Pattern size (extent, number of elements) in every dimension
-    const SizeSpec_t &         sizespec,
+    const SizeSpec_t         sizespec,
     /// Distribution type (BLOCKED, CYCLIC, BLOCKCYCLIC or NONE).
     /// Defaults to BLOCKED.
-    const DistributionSpec_t & dist     = DistributionSpec_t(),
+    const DistributionSpec_t dist     = DistributionSpec_t(),
     /// Cartesian arrangement of units within the team
-    const TeamSpec_t &         teamspec = TeamSpec_t::TeamSpec(),
+    const TeamSpec_t         teamspec = TeamSpec_t::TeamSpec(),
     /// Team containing units to which this pattern maps its elements
-    dash::Team &               team     = dash::Team::All())
+    dash::Team &             team     = dash::Team::All())
   : _size(sizespec.size()),
     _memory_layout(std::array<SizeType, 1> {{ _size }}),
     _distspec(dist),
@@ -291,11 +291,11 @@ public:
    */
   Pattern(
     /// Pattern size (extent, number of elements) in every dimension
-    const SizeSpec_t &         sizespec,
+    const SizeSpec_t         sizespec,
     /// Distribution type (BLOCKED, CYCLIC, BLOCKCYCLIC, TILE or NONE) of
     /// all dimensions. Defaults to BLOCKED in first, and NONE in higher
     /// dimensions
-    const DistributionSpec_t & dist = DistributionSpec_t(),
+    const DistributionSpec_t dist = DistributionSpec_t(),
     /// Team containing units to which this pattern maps its elements
     Team &                     team = dash::Team::All())
   : _size(sizespec.size()),
