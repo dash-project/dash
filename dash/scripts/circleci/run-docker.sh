ce8adb387a0b5a647d3046b00dfacf8e6f1a6236
Felix Moessbauer <felix.moessbauer@campus.lmu.de>
2016-10-27 21:18:26 +0200

    moved docker command to own file

---
+++ dash/scripts/circleci/run-docker.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+echo "Starting docker container ..."
+
+docker run -v $PWD:/opt/dash dash/testing /bin/sh -c "export DASH_MAX_UNITS='4'; export GTEST_TOTAL_SHARDS=${CIRCLE_NODE_TOTAL}; export GTEST_SHARD_INDEX=${CIRCLE_NODE_INDEX}; sh dash/scripts/dash-ci.sh | grep -v 'LOG =' | tee dash-ci.log 2> dash-ci.err;":
+
+echo "checking logs"
+
+cat $PWD/dash-ci.log | grep "FAILED"
+if [ "$?" -eq "0" ]; then
+  echo "Log contains errors"
+  exit 1
+fi
