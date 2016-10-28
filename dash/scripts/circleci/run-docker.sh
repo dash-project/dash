985f3e82493c88230a744397f969f54ca33470d7
Felix Moessbauer <felix.moessbauer@campus.lmu.de>
2016-10-28 10:20:55 +0200

    copy test results to circleci build aretefacts, using one dir per target

--- dash/scripts/circleci/run-docker.sh
+++ dash/scripts/circleci/run-docker.sh
@@ -4,7 +4,10 @@ echo "Starting docker container ..."
 
 docker run -v $PWD:/opt/dash dash/testing /bin/sh -c "export DASH_MAX_UNITS='4'; export GTEST_TOTAL_SHARDS=${CIRCLE_NODE_TOTAL}; export GTEST_SHARD_INDEX=${CIRCLE_NODE_INDEX}; sh dash/scripts/dash-ci.sh | grep -v 'LOG =' | tee dash-ci.log 2> dash-ci.err;"
 
-cp ./build-ci/*/*/dash-tests-*.xml $CIRCLE_TEST_REPORTS/
+TARGETS=`ls -d ./build-ci/*/* | xargs -n1 basename`
+for TARGET in ${TARGETS[@]}; do
+  cp ./build-ci/*/$TARGET/dash-tests-*.xml $CIRCLE_TEST_REPORTS/$TARGET/
+done
 
 echo "checking logs"
 
