37cc953d0a6ec68179461cfa1cee465fdd1451a9
Felix Moessbauer <felix.moessbauer@campus.lmu.de>
2016-10-30 12:03:54 +0100

    limit number of parallel builds to value set in DASH_MAKE_PROCS

--- dash/scripts/dash-ci-deploy.sh
+++ dash/scripts/dash-ci-deploy.sh
@@ -13,9 +13,9 @@ BUILD_TYPE="Release"
 MAKE_TARGET=""
 MAKE_PROCS=`cat /proc/cpuinfo | grep --count 'processor'`
 if ! [ "$DASH_MAKE_PROCS" = "" ]; then
-  if [ "$MAKE_PROCS" -gt "4" ]; then
+  if [ "$MAKE_PROCS" -gt "$DASH_MAKE_PROCS" ]; then
     echo "Use at most $MAKE_PROCS processors for building"
-    MAKE_PROCS="4"
+    MAKE_PROCS="$DASH_MAKE_PROCS"
   fi
 fi
 
