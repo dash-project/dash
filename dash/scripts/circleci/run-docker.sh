ba82ce075a7869069ac776456a82db1d8c109698
Felix Moessbauer <felix.moessbauer@campus.lmu.de>
2016-11-02 21:05:07 +0100

    run CI tests for mpich and openmpi with 4 units on circleci

--- dash/scripts/circleci/run-docker.sh
+++ dash/scripts/circleci/run-docker.sh
@@ -1,13 +1,22 @@
 #!/bin/sh
 
-echo "Starting docker container ..."
+if [ "${CIRCLE_NODE_INDEX}" -eq "0" ]; then
+  MPIENV="openmpi"
+elif [ "${CIRCLE_NODE_INDEX}" -eq "1" ]; then 
+  MPIENV="mpich"
+else
+  echo "more than two parallel builds are not supported"
+  return 0
+fi
 
-docker run -v $PWD:/opt/dash dash/testing /bin/sh -c "export DASH_MAKE_PROCS='4'; export DASH_MAX_UNITS='2'; export GTEST_TOTAL_SHARDS=${CIRCLE_NODE_TOTAL}; export GTEST_SHARD_INDEX=${CIRCLE_NODE_INDEX}; sh dash/scripts/dash-ci.sh | grep -v 'LOG =' | tee dash-ci.log 2> dash-ci.err;"
+echo "Starting docker container: $MPIENV"
+
+docker run -v $PWD:/opt/dash dash/ci-$MPIENV /bin/sh -c "export DASH_MAKE_PROCS='4'; export DASH_MAX_UNITS='4'; sh dash/scripts/dash-ci.sh | grep -v 'LOG =' | tee dash-ci.log 2> dash-ci.err;"
 
 TARGETS=`ls -d ./build-ci/*/* | xargs -n1 basename`
 for TARGET in $TARGETS; do
-  mkdir -p $CIRCLE_TEST_REPORTS/$TARGET
-  cp ./build-ci/*/$TARGET/dash-tests-*.xml $CIRCLE_TEST_REPORTS/$TARGET/
+  mkdir -p $CIRCLE_TEST_REPORTS/$MPIENV/$TARGET
+  cp ./build-ci/*/$TARGET/dash-tests-*.xml $CIRCLE_TEST_REPORTS/$MPIENV/$TARGET/
 done
 
 echo "checking logs"
