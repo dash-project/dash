bf187094db7b035f516fcd585fa7b82b11c43a14
Felix Moessbauer <felix.moessbauer@campus.lmu.de>
2016-11-02 21:49:48 +0100

    seperate steps build and test to reduce output per step

---
+++ dash/scripts/circleci/build-docker.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+MPIENVS=(openmpi mpich)
+
+# build docker containers
+
+i=0
+for MPIENV in ${MPIENVS[@]}; do
+  if [[ "$i % ${CIRCLE_NODE_TOTAL} == ${CIRCLE_NODE_INDEX}" ]]; then
+    echo "Building docker container $MPIENV"
+    docker build --rm=false --tag=dash/ci-$MPIENV ./dash/scripts/docker-testing/$MPIENV/
+  fi
+  i=$((i + 1))
+done
+exit 0
--- dash/scripts/circleci/run-docker.sh
+++ dash/scripts/circleci/run-docker.sh
@@ -2,18 +2,6 @@
 
 MPIENVS=(openmpi mpich)
 
-# build docker containers
-
-i=0
-for MPIENV in ${MPIENVS[@]}; do
-  if [[ "$i % ${CIRCLE_NODE_TOTAL} == ${CIRCLE_NODE_INDEX}" ]]; then
-    echo "Building docker container $MPIENV"
-    docker build --rm=false --tag=dash/ci-$MPIENV ./dash/scripts/docker-testing/$MPIENV/
-  fi
-  i=$((i + 1))
-done
-exit 0
-
 # run tests 
 i=0
 for MPIENV in ${MPIENVS[@]}; do
