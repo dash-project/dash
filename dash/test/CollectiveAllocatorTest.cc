83d76aade1e5f934738ddcf5ee59573b603e7a88
Clemens Manert <Clemens@Manert.de>
2016-11-02 10:16:48 +0100

    Added Testcase for CollectiveAllocator's constructor.

--- dash/test/CollectiveAllocatorTest.cc
+++ dash/test/CollectiveAllocatorTest.cc
@@ -3,3 +3,13 @@
 #include <libdash.h>
 #include <gtest/gtest.h>
 #include "TestBase.h"
+
+TEST_F(CollectiveAllocatorTest, Constructor)
+{
+  dash::allocator::CollectiveAllocator<int> target = dash::allocator::CollectiveAllocator<int>();
+
+  dart_gptr_t requested = target.allocate(sizeof(int) * 10);
+
+  ASSERT_EQ(dash::myid(), requested.unitid);
+}
+
