838bc197de55d5a5738f7492c514aaadf1f270d2
Felix Moessbauer <felix.moessbauer@campus.lmu.de>
2016-11-02 22:28:30 +0100

    MPICH and OpenMPI testing environment (#51)
    * run CI tests for mpich and openmpi with 4 units on circleci
    * parallelize mpi version tests on a flexible number of test runners

--- dash/scripts/circleci/run-docker.sh
+++ dash/scripts/circleci/run-docker.sh
@@ -1,19 +1,30 @@
-#!/bin/sh
+#!/bin/bash
 
-echo "Starting docker container ..."
+MPIENVS=(openmpi mpich)
 
-docker run -v $PWD:/opt/dash dash/testing /bin/sh -c "export DASH_MAKE_PROCS='4'; export DASH_MAX_UNITS='2'; export GTEST_TOTAL_SHARDS=${CIRCLE_NODE_TOTAL}; export GTEST_SHARD_INDEX=${CIRCLE_NODE_INDEX}; sh dash/scripts/dash-ci.sh | grep -v 'LOG =' | tee dash-ci.log 2> dash-ci.err;"
-
-TARGETS=`ls -d ./build-ci/*/* | xargs -n1 basename`
-for TARGET in $TARGETS; do
-  mkdir -p $CIRCLE_TEST_REPORTS/$TARGET
-  cp ./build-ci/*/$TARGET/dash-tests-*.xml $CIRCLE_TEST_REPORTS/$TARGET/
+# run tests 
+i=0
+for MPIENV in ${MPIENVS[@]}; do
+  if [[ $(( $i % ${CIRCLE_NODE_TOTAL} )) -eq ${CIRCLE_NODE_INDEX} ]]; then
+    
+    echo "Starting docker container: $MPIENV"
+    
+    docker run -v $PWD:/opt/dash dash/ci-$MPIENV /bin/sh -c "export DASH_MAKE_PROCS='4'; export DASH_MAX_UNITS='4'; sh dash/scripts/dash-ci.sh | grep -v 'LOG =' | tee dash-ci.log 2> dash-ci.err;"
+    
+    TARGETS=`ls -d ./build-ci/*/* | xargs -n1 basename`
+    for TARGET in $TARGETS; do
+      mkdir -p $CIRCLE_TEST_REPORTS/$MPIENV/$TARGET
+      cp ./build-ci/*/$TARGET/dash-tests-*.xml $CIRCLE_TEST_REPORTS/$MPIENV/$TARGET/
+    done
+    
+    echo "checking logs"
+    
+    cat $PWD/dash-ci.log | grep "FAILED"
+    if [ "$?" -eq "0" ]; then
+      echo "Log contains errors"
+      exit 1
+    fi
+  fi
+  
+  i=$((i + 1))
 done
-
-echo "checking logs"
-
-cat $PWD/dash-ci.log | grep "FAILED"
-if [ "$?" -eq "0" ]; then
-  echo "Log contains errors"
-  return 1
-fi
