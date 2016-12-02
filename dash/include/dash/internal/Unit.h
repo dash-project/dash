4846676416a01aff17f5099aab454e2076315cd9
Joseph Schuchart <joseph.schuchart@hlrs.de>
2016-12-02 14:11:00 +0100

    Implement a generic unitid struct and operations on it; provide specialization for global and local unit ID

---
+++ dash/include/dash/internal/Unit.h
@@ -0,0 +1,167 @@
+#ifndef DASH__UNIT_H_
+#define DASH__UNIT_H_
+
+/**
+ * Implementation of typed unit IDs.
+ */
+
+
+#include <dash/dart/if/dart_types.h>
+
+namespace dash {
+namespace internal {
+namespace unit {
+
+enum unit_scope {
+  local_unit,
+  global_unit
+};
+
+template <unit_scope IdScope>
+struct unit_id {
+  /** default initialization to zero */
+  constexpr explicit unit_id() noexcept : id(0) {}
+
+  /**
+   * initialization using a dart_unit_t
+   * Note that it is explicit to prevent implicit conversion from existing dart_unit_t variables.
+   * However, it prevents the following from working:
+   * dash::global_unitid unit = 0;
+   * Instead, use:
+   * dash::global_unitid unit{0};
+   */
+  constexpr explicit unit_id(dart_unit_t id) noexcept : id(id) {}
+
+  /** copy-ctor: explicit conversion from another unit_id type */
+  template<unit_scope S>
+  constexpr explicit unit_id<IdScope>(const unit_id<S>& uid) noexcept : id(uid.id) {}
+
+  /** TODO [JS]: the following are not constexpr since C++11 does not allow
+   *             for multi-statement constexpr functions (C++14 does).
+   */
+  const unit_id<IdScope> operator=(unit_id<IdScope> uid) noexcept {
+    this->id = uid.id;
+    return *this;
+  }
+  const unit_id operator=(dart_unit_t id) noexcept {
+    this->id = id;
+    return *this;
+  }
+  template<
+    typename T,
+    typename std::enable_if<
+               std::is_integral<T>::value >::type = 0>
+  const unit_id operator+=(T id) noexcept {
+    this->id += id;
+    return *this;
+  }
+  template<
+    typename T,
+    typename std::enable_if<
+               std::is_integral<T>::value >::type = 0>
+  const unit_id operator-=(T id) noexcept {
+    this->id -= id;
+    return *this;
+  }
+  template<
+    typename T,
+    typename std::enable_if<
+               std::is_integral<T>::value >::type = 0>
+  const unit_id operator*=(T id) noexcept {
+    this->id *= id;
+    return *this;
+  }
+  template<
+    typename T,
+    typename std::enable_if<
+               std::is_integral<T>::value >::type = 0>
+  const unit_id operator/=(T id) noexcept {
+    this->id /= id;
+    return *this;
+  }
+
+  const unit_id operator++() noexcept {
+    this->id++;
+    return *this;
+  }
+  const unit_id operator--() noexcept {
+    this->id--;
+    return *this;
+  }
+  const unit_id operator++(int) noexcept {
+    this->id++;
+    return *this;
+  }
+  const unit_id operator--(int) noexcept {
+    this->id--;
+    return *this;
+  }
+
+  /** allow cast to dart_unit_t */
+  constexpr operator dart_unit_t() noexcept { return id; }
+
+  /** address-of is the address of the unit ID */
+  dart_unit_t* operator&() {
+    return &this->id;
+  }
+
+  /** address-of is the address of the unit ID */
+  dart_unit_t const* operator&() const {
+    return &this->id;
+  }
+
+  dart_unit_t id;
+};
+
+template <unit_scope IdScope>
+constexpr unit_id<IdScope> operator+(unit_id<IdScope> lhs, unit_id<IdScope> rhs) {
+  return unit_id<IdScope>(lhs.id + rhs.id);
+}
+
+template <unit_scope IdScope>
+constexpr unit_id<IdScope> operator-(unit_id<IdScope> lhs, unit_id<IdScope> rhs) {
+  return unit_id<IdScope>(lhs.id - rhs.id);
+}
+
+template <unit_scope IdScope>
+constexpr unit_id<IdScope> operator/(dart_unit_t lhs, unit_id<IdScope> rhs) {
+  return unit_id<IdScope>(lhs / rhs.rhs);
+}
+
+template <unit_scope IdScope>
+constexpr unit_id<IdScope> operator/(unit_id<IdScope> lhs, dart_unit_t rhs) {
+  return unit_id<IdScope>(lhs.id / rhs);
+}
+
+template <unit_scope IdScope>
+constexpr bool operator==(unit_id<IdScope> lhs, unit_id<IdScope> rhs) {
+  return lhs.id == rhs.id;
+}
+
+
+template <unit_scope IdScope>
+constexpr bool operator!=(unit_id<IdScope> lhs, unit_id<IdScope> rhs) {
+  return !(lhs == rhs);
+}
+
+template <unit_scope IdScope>
+constexpr bool operator<(unit_id<IdScope> lhs, unit_id<IdScope> rhs) {
+  return lhs.id < rhs.id;
+}
+
+template <unit_scope IdScope>
+constexpr bool operator>(unit_id<IdScope> lhs, unit_id<IdScope> rhs) {
+  return lhs.id > rhs.id;
+}
+
+template<unit_scope IdScope>
+std::ostream & operator<<(std::ostream& os, unit_id<IdScope> id)
+{
+  return os<<id.id;
+}
+
+} // namespace unit
+} // namespace internal
+} // namespace dash
+
+#endif
