e9a28577b0f044477bc92a84dd4e65c862c1ea68
Stefan Effenberger <pstiefn@gmail.com>
2016-12-12 17:31:26 +0100

    added empty partition concept

---
+++ dash/include/dash/Partition.h
@@ -0,0 +1,27 @@
+#ifndef DASH__PARTITION_H_
+#define  DASH__PARTITION_H_
+
+namespace dash {
+
+  /**
+   * \defgroup DashPartitionConcept Graph Partition Concept
+   * Partitioning concept for distributed graph data (vertices and edges).
+   * 
+   * \ingroup DashConcept
+   * \{
+   * \par Description
+   * 
+   * Description here.
+   * 
+   * \par Methods
+   * 
+   * \}
+   */
+
+}
+
+#endif // DASH__PARTITION_H_
+
+#include <dash/graph/partition/VertexPartition.h>
+#include <dash/graph/partition/EdgePartition.h>
+#include <dash/graph/partition/LoadBalancePartition.h>
