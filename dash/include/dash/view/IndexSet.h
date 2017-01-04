a299650a4b246f7d808ebac0af33c7b5befbbf08
Tobias Fuchs <fuchst@nm.ifi.lmu.de>
2017-01-04 11:36:15 +0100

    Implementing index set types

--- dash/include/dash/view/IndexSet.h
+++ dash/include/dash/view/IndexSet.h
@@ -1,6 +1,10 @@
 #ifndef DASH__VIEW__INDEX_SET_H__INCLUDED
 #define DASH__VIEW__INDEX_SET_H__INCLUDED
 
+#include <dash/view/ViewTraits.h>
+#include <dash/view/Origin.h>
+
+
 namespace dash {
 
 template <
@@ -31,6 +35,10 @@ public:
 public:
   typedef index_iterator iterator;
 
+  IndexSetBase(const ViewType & view)
+  : _domain(view)
+  { }
+
   constexpr iterator begin() const {
     return iterator(*static_cast<const IndexSetType *>(this), 0);
   }
@@ -40,6 +48,13 @@ public:
                     static_cast<const IndexSetType *>(this)->size());
   }
 
+protected:
+  constexpr const ViewType & domain() const {
+    return _domain;
+  }
+
+private:
+  const ViewType & _domain;
 };
 
 template <class ViewType>
@@ -55,7 +70,9 @@ public:
     const ViewType   & view,
     index_type         begin,
     index_type         end)
-  : _domain(view), _domain_begin_idx(begin), _domain_end_idx(end)
+  : base_t(view),
+    _domain_begin_idx(begin),
+    _domain_end_idx(end)
   { }
 
   constexpr index_type operator[](index_type image_index) const {
@@ -67,9 +84,40 @@ public:
   }
 
 private:
-  const ViewType & _domain;
-  index_type       _domain_begin_idx;
-  index_type       _domain_end_idx;
+  index_type _domain_begin_idx;
+  index_type _domain_end_idx;
+};
+
+template <class ViewType>
+class IndexSetLocal
+: IndexSetBase<IndexSetLocal<ViewType>, ViewType>
+{
+  typedef IndexSetLocal<ViewType>                               self_t;
+  typedef IndexSetBase<self_t, ViewType>                        base_t;
+
+  typedef typename dash::view_traits<ViewType>::origin_type   origin_t;
+  typedef typename origin_t::pattern_type                    pattern_t;
+public:
+  typedef typename ViewType::index_type                     index_type;
+
+  IndexSetLocal(const ViewType & view)
+  : base_t(view)
+  { }
+
+private:
+  constexpr const pattern_t & pattern() const {
+    return dash::origin(this->domain()).pattern();
+  }
+
+public:
+
+  constexpr index_type operator[](index_type image_index) const {
+    return pattern().local_index(image_index);
+  }
+
+  constexpr index_type size() const {
+    return static_cast<index_type>(pattern().local_size());
+  }
 };
 
 } // namespace dash
