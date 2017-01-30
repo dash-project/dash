62354461887acb414f4bb6bad8ea7406046b3efc
Stefan Effenberger <pstiefn@gmail.com>
2017-01-30 17:26:27 +0100

    updated graph interface

--- dash/include/dash/Graph.h
+++ dash/include/dash/Graph.h
@@ -5,7 +5,7 @@ namespace dash {
 
 /**
  * \defgroup DashGraphConcept Graph Concept
- * Concept of a statically distributed graph container.
+ * Concept of a distributed graph container.
  * 
  * \ingroup DashContainerConcept
  * \{
@@ -57,49 +57,55 @@ class VertexIterator {
 
 }
 
+/**
+ * Wrapper for the vertex iterators of the graph.
+ */
 struct VertexIteratorWrapper {
 
-  typedef VertexIterator vertex_iterator;
+  typedef VertexIterator             iterator;
+  typedef const VertexIterator       const_iterator;
+  typedef LocalVertexIterator        local_iterator;
+  typedef const LocalVertexIterator  const_local_iterator;
   
   /**
    * Returns global iterator to the beginning of the vertex list.
    */
-  vertex_iterator begin();
+  iterator begin();
   
   /**
    * Returns global iterator to the beginning of the vertex list.
    */
-  const_vertex_iterator begin() const;
+  const_iterator begin() const;
   
   /**
    * Returns global iterator to the end of the vertex list.
    */
-  vertex_iterator end();
+  iterator end();
   
   /**
    * Returns global iterator to the end of the vertex list.
    */
-  const_vertex_iterator end() const;
+  const_iterator end() const;
   
   /**
    * Returns local iterator to the beginning of the vertex list.
    */
-  vertex_iterator lbegin();
+  local_iterator lbegin();
   
   /**
    * Returns local iterator to the beginning of the vertex list.
    */
-  const_vertex_iterator lbegin() const;
+  const_local_iterator lbegin() const;
   
   /**
    * Returns local iterator to the end of the vertex list.
    */
-  vertex_iterator lend();
+  local_iterator lend();
   
   /**
    * Returns local iterator to the end of the vertex list.
    */
-  const_vertex_iterator lend() const;
+  const_local_iterator lend() const;
 
 };
 
@@ -107,12 +113,14 @@ struct VertexIteratorWrapper {
 
 }
 
+/**
+ * Distributed, dynamic graph container for sparse graphs.
+ */
 template<
   GraphDirection Direction  = dash::graph::DirectedGraph,
   typename DynamicPattern   = dash::graph::VertexPartitionedDynamicPattern,
   typename VertexProperties = Empty,           // user-defined struct
   typename EdgeProperties   = Empty,           // user-defined struct
-  typename GraphProperties  = Empty,           // user-defined struct
   typename VertexContainer  = std::vector,
   typename EdgeContainer    = std::vector,
   typename VertexSizeType   = std::size_t,
@@ -120,15 +128,18 @@ template<
 class Graph {
 
   typedef detail::VertexIteratorWrapper               vertex_it_wrapper;
+  typedef detail::EdgeIteratorWrapper                 edge_it_wrapper;
+  typedef detail::InEdgeIteratorWrapper               in_edge_it_wrapper;
+  typedef detail::OutEdgeIteratorWrapper              out_edge_it_wrapper;
+  typedef detail::AdjacencyIteratorWrapper            adjacency_it_wrapper;
 
 public:
 
-  typedef detail::vertex                              vertex_descriptor;
-  typedef detail::edge<Direction, vertex_descriptor>  edge_descriptor;
+  typedef typename detail::vertex::index_type         vertex_index_type;
+  typedef typename detail::edge<Direction, 
+          vertex_index_type>::index_type              edge_index_type;
   typedef VertexSizeType                              vertex_size_type;
-  typedef vertex_size_type                            vertex_index_type;
   typedef EdgeSizeType                                edge_size_type;
-  typedef edge_size_type                              edge_index_type;
 
   typedef Direction                                   direction_type;
   typedef DynamicPattern                              pattern_type;
@@ -137,15 +148,19 @@ public:
   typedef VertexProperties                            vertex_properties_type;
   typedef EdgeProperties                              edge_properties_type;
 
-  typedef vertex_it_wrapper::vertex_iterator          vertex_iterator;
-  typedef                                             edge_iterator;
-  typedef                                             in_edge_iterator;
-  typedef                                             out_edge_iterator;
-  typedef                                             adjacency_iterator;
+  typedef typename vertex_it_wrapper::iterator        vertex_iterator;
+  typedef typename edge_it_wrapper::iterator          edge_iterator;
+  typedef typename in_edge_it_wrapper::iterator       in_edge_iterator;
+  typedef typename out_edge_it_wrapper::iterator      out_edge_iterator;
+  typedef typename adjacency_it_wrapper::iterator     adjacency_iterator;
 
 public:
 
-  vertex_it_wrapper vertices;
+  vertex_it_wrapper     vertices;
+  edge_it_wrapper       edges;
+  in_edge_it_wrapper    in_edges;
+  out_edge_it_wrapper   out_edges;
+  adjacency_it_wrapper  adjacent_vertices;
 
 public:
 
@@ -169,20 +184,13 @@ public:
   /**
    * Returns a property object for the given vertex.
    */
-  VertexProperties & operator[](const vertex_descriptor & v) const;
+  VertexProperties & operator[](const vertex_index_type & v) const;
 
   /**
    * Returns a property object for the given edge.
    */
-  EdgeProperties & operator[](const edge_descriptor & e) const;
+  EdgeProperties & operator[](const edge_index_type & e) const;
 
-  /**
-   * TODO: Return full property object for graph properties or
-   *       use getter- and setter methods for individual fields?
-   *       Also: Are graph properties really needed? The user could just
-   *       save them elsewhere.
-   */
-  
   /**
    * Returns the number of vertices in the whole graph.
    */
@@ -212,25 +220,25 @@ public:
    * Adds a vertex with default properties.
    * NOTE: global method.
    */
-  vertex_descriptor add_vertex();
+  vertex_index_type add_vertex();
 
   /**
    * Adds a vertex with the given properties.
    * NOTE: global method.
    */
-  vertex_descriptor add_vertex(const VertexProperties & prop);
+  vertex_index_type add_vertex(const VertexProperties & prop);
 
   /**
    * Removes a given vertex.
    * NOTE: global method.
    */
-  void remove_vertex(const vertex_descriptor & v);
+  void remove_vertex(const vertex_index_type & v);
 
   /**
    * Removes all edges (in & out) from the given vertex).
    * NOTE: global method.
    */
-  void clear_vertex(vertex_descriptor & v);
+  void clear_vertex(vertex_index_type & v);
 
   /**
    * Adds an edge between two given vertices with default properties.
@@ -239,29 +247,29 @@ public:
    * \return pair, with pair::first set to the edge_descriptor of the added 
    * edge 
    */
-  std::pair<edge_descriptor, bool> add_edge(const vertex_descriptor & v1, 
-      const vertex_descriptor & v2);
+  std::pair<edge_index_type, bool> add_edge(const vertex_index_type & v1, 
+      const vertex_index_type & v2);
 
   /**
    * Adds an edge between two given vertices with the given properties 
    * locally.
    * NOTE: global method.
    */
-  std::pair<edge_descriptor, bool> add_edge(const vertex_descriptor & v1, 
-      const vertex_descriptor & v2, const EdgeProperties & prop);
+  std::pair<edge_index_type, bool> add_edge(const vertex_index_type & v1, 
+      const vertex_index_type & v2, const EdgeProperties & prop);
 
   /**
    * Removes an edge between two given vertices.
    * NOTE: global method.
    */
-  void remove_edge(const vertex_descriptor & v1, 
-      const vertex_descriptor & v2);
+  void remove_edge(const vertex_index_type & v1, 
+      const vertex_index_type & v2);
 
   /**
-   * Removes a given edge.
+   * Removes a given edge.a9f5c03d9de8dda4
    * NOTE: global method.
    */
-  void remove_edge(const edge_descriptor & e);
+  void remove_edge(const edge_index_type & e);
 
   /**
    * Commits local changes performed by methods classified as "global" to
