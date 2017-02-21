ff9d2749ea9de77d5d7e074b5ce4c07d63ebf755
Tobias Fuchs <fuchst@nm.ifi.lmu.de>
2017-02-21 18:45:27 +0100

    Added missing const-conversion in LocalMatrixRef.sub

--- dash/examples/ex.12.feat-283/main.cpp
+++ dash/examples/ex.12.feat-283/main.cpp
@@ -5,53 +5,49 @@ using std::cin;
 using std::endl;
 using std::vector;
 
-using uint  = unsigned int;
+using uint = unsigned int;
 
-inline void sum(         
-                   const uint   nelts ,
-  const dash::NArray<uint, 2> & matIn, //if this parameter is const then there is @problem
-                   const uint   myid
-  ){
-   uint lclRows = matIn.pattern().local_extents()[0];
-   
-   uint const * mPtr;
-   uint localSum = 0;
-   
-   dash::barrier( );
-   
-   /* if the NArray/Matrix is a non const paremter everything compiles and runs fine.
-    * is the NArray/Matrix a const parameter then the compiler throws an error
-    */
-   
-   for(uint i = 0; i < lclRows; ++i){
-     mPtr = matIn.local.row(i).lbegin(); //here is the problem at `row(i)`
-     
-     for(uint j = 0; j < nelts ; ++j){
+inline void sum(const uint nelts,
+                const dash::NArray<uint, 2> &matIn,
+                const uint myid) {
+  uint lclRows = matIn.pattern().local_extents()[0];
+
+  uint const *mPtr;
+  uint localSum = 0;
+
+  dash::barrier();
+
+  /* If the NArray/Matrix is a non const paremter everything compiles and
+   * runs fine.
+   * If the NArray/Matrix a const parameter then the compiler throws an
+   * error.
+   */
+
+  for (uint i = 0; i < lclRows; ++i) {
+    mPtr = matIn.local.row(i).lbegin();
+
+    for (uint j = 0; j < nelts; ++j) {
       localSum += *(mPtr++);
-     }
-   }
+    }
+  }
 }
 
+int main(int argc, char *argv[]) {
+  dash::init(&argc, &argv);
+  uint myid = static_cast<uint>(dash::Team::GlobalUnitID().id);
 
-int main( int argc, char* argv[] )
-{  
-  dash::init( &argc,&argv );
-  uint myid = static_cast<uint>( dash::Team::GlobalUnitID( ).id );
-  
   uint nelts = 40;
-  
+
   dash::NArray<uint, 2> mat(nelts, nelts);
-  
-  //init matrix with some values
+
+  // init matrix with some values
   uint counter = myid + 20;
-  if( 0 == myid ) {
-    for(uint * i = mat.lbegin( ); i < mat.lend( ); ++i) {
+  if (0 == myid) {
+    for (uint *i = mat.lbegin(); i < mat.lend(); ++i) {
       *i = ++counter;
     }
   }
-  
-  sum(nelts, mat, myid);  
-  dash::finalize( );
+
+  sum(nelts, mat, myid);
+  dash::finalize();
 }
-
-
