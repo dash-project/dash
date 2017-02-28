567353dbcb1094711d805df8e841ae5b2f833ca3
Felix Moessbauer <felix.moessbauer@gmail.com>
2017-02-28 19:51:08 +0100

    add support for array types

--- dash/include/dash/Co_array.h
+++ dash/include/dash/Co_array.h
@@ -1,6 +1,8 @@
 #ifndef COARRAY_H_INCLUDED
 #define COARRAY_H_INCLUDED
 
+#include <type_traits>
+
 #include <dash/GlobRef.h>
 #include <dash/iterator/GlobIter.h>
 
@@ -27,31 +29,77 @@
 
 namespace dash {
 
+// forward declaration
+template<
+  typename ElementType,
+  typename IndexType,
+  class    PatternType >
+class Array;
+
+template<
+  typename ElementType,
+  dim_t    NumDimensions,
+  typename IndexType,
+  class    PatternType>
+class Matrix;
+
+
+/**
+ * fortran style co_array
+ */
 template<
   typename T,
   typename IndexType = dash::default_index_t>
 class Co_array {
 private:
-  using _pattern_type   = dash::BlockPattern<1>;
+  
+  // storage type dispatching
+  template<
+    typename __element_type,
+    typename __index_type,
+    typename __pattern_type,
+    int      __rank>
+  struct _get_storage_type { 
+    typedef Matrix<__element_type, __rank+1, __index_type, __pattern_type> type;
+  };
+  
+  template<
+    typename __element_type,
+    typename __index_type,
+    typename __pattern_type>
+  struct _get_storage_type<__element_type, __index_type, __pattern_type, 0> { 
+    typedef Array<__element_type, __index_type, __pattern_type> type;
+  };
+  
+private:
+  static constexpr int _rank = std::rank<T>::value;
+  
+  using _index_type     = typename std::make_unsigned<IndexType>::type;
+  using _pattern_type   = dash::BlockPattern<_rank+1>;
+  using _element_type   = typename std::remove_all_extents<T>::type;
+  using _storage_type   = typename _get_storage_type<_element_type,
+                                                     _index_type,
+                                                     _pattern_type,
+                                                     _rank>::type;
 
 public:
   // Types
-  using value_type             = T;
+  using value_type             = _element_type;
   using difference_type        = IndexType;
   using index_type             = IndexType;
-  using size_type              = typename std::make_unsigned<IndexType>::type;
-  using iterator               = GlobIter<T, _pattern_type>; 
-  using const_iterator         = GlobIter<const T, _pattern_type>; 
-  using reverse_iterator       = GlobIter<T, _pattern_type>; 
-  using const_reverse_iterator = GlobIter<const T, _pattern_type>; 
-  using reference              = GlobRef<T>;
-  using const_reference        = GlobRef<T>;
-  using local_pointer          = T *;
-  using const_local_pointer    = const T *;
+  using size_type              = _index_type;
+  using iterator               = GlobIter<_element_type, _pattern_type>; 
+  using const_iterator         = GlobIter<const _element_type, _pattern_type>; 
+  using reverse_iterator       = GlobIter<_element_type, _pattern_type>; 
+  using const_reverse_iterator = GlobIter<const _element_type, _pattern_type>; 
+  using reference              = GlobRef<_element_type>;
+  using const_reference        = GlobRef<_element_type>;
+  using local_pointer          = _element_type *;
+  using const_local_pointer    = const _element_type *;
   //using view_type              = Co_arrayRef<>;
   //using local_type             = LocalCo_arrayRef<>;
   using pattern_type           = _pattern_type;
-  
+
 };
 
 } // namespace dash
--- dash/test/CoArrayTest.cc
+++ dash/test/CoArrayTest.cc
@@ -1,28 +1,57 @@
 #include "TestBase.h"
 #include "CoArrayTest.h"
 
+#include <type_traits>
+
 
 TEST_F(CoArrayTest, TypesInterface)
 {
   dash::Co_array<int>         i;
-  //dash::Co_array<int[10][20]> x;
+  dash::Co_array<int[10][20]> x;
   //dash::Co_array<int[][20]>   y(n);
   
-  using value_type             = decltype(i)::value_type;
-  using difference_type        = decltype(i)::difference_type;
-  using index_type             = decltype(i)::index_type;
-  using size_type              = decltype(i)::size_type;
-  using iterator               = decltype(i)::iterator;
-  using const_iterator         = decltype(i)::const_iterator;
-  using reverse_iterator       = decltype(i)::reverse_iterator;
-  using const_reverse_iterator = decltype(i)::const_reverse_iterator;
-  using reference              = decltype(i)::reference;
-  using const_reference        = decltype(i)::const_reference;
-  using local_pointer          = decltype(i)::local_pointer;
-  using const_local_pointer    = decltype(i)::const_local_pointer;
-  //using view_type              = decltype(i)::view_type;
-  //using local_type             = decltype(i)::local_type;
-  using pattern_type           = decltype(i)::pattern_type;
+  // check scalar case
+  {
+    using value_type             = decltype(i)::value_type;
+    using difference_type        = decltype(i)::difference_type;
+    using index_type             = decltype(i)::index_type;
+    using size_type              = decltype(i)::size_type;
+    using iterator               = decltype(i)::iterator;
+    using const_iterator         = decltype(i)::const_iterator;
+    using reverse_iterator       = decltype(i)::reverse_iterator;
+    using const_reverse_iterator = decltype(i)::const_reverse_iterator;
+    using reference              = decltype(i)::reference;
+    using const_reference        = decltype(i)::const_reference;
+    using local_pointer          = decltype(i)::local_pointer;
+    using const_local_pointer    = decltype(i)::const_local_pointer;
+    //using view_type              = decltype(i)::view_type;
+    //using local_type             = decltype(i)::local_type;
+    using pattern_type           = decltype(i)::pattern_type;
+    
+    static_assert(std::rank<value_type>::value == 0,
+                  "base type must have rank 0");
+  }
+  // check fully specified array case
+  {
+    using value_type             = decltype(x)::value_type;
+    using difference_type        = decltype(x)::difference_type;
+    using index_type             = decltype(x)::index_type;
+    using size_type              = decltype(x)::size_type;
+    using iterator               = decltype(x)::iterator;
+    using const_iterator         = decltype(x)::const_iterator;
+    using reverse_iterator       = decltype(x)::reverse_iterator;
+    using const_reverse_iterator = decltype(x)::const_reverse_iterator;
+    using reference              = decltype(x)::reference;
+    using const_reference        = decltype(x)::const_reference;
+    using local_pointer          = decltype(x)::local_pointer;
+    using const_local_pointer    = decltype(x)::const_local_pointer;
+    //using view_type              = decltype(x)::view_type;
+    //using local_type             = decltype(x)::local_type;
+    using pattern_type           = decltype(x)::pattern_type;
+    
+    static_assert(std::rank<value_type>::value == 0,
+                  "base type must have rank 0");
+  }
 }
 TEST_F(CoArrayTest, ContainerInterface)
 {
