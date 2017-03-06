bb3dae4ee69c90909d4be26e3f09998918c2f720
Joseph Schuchart <joseph.schuchart@hlrs.de>
2017-03-06 16:22:22 +0100

    Add definition of DART_API

---
+++ dart-if/v3.2/include/dash/dart/if/dart_util.h
@@ -0,0 +1,13 @@
+#ifndef DART_IF_DART_UTIL_H_
+#define DART_IF_DART_UTIL_H_
+
+
+/** \cond DART_HIDDEN_SYMBOLS */
+/**
+ * Mark a function (or variable) as part of the DART API.
+ */
+#define DART_API       __attribute__ ((visibility ("default")))
+/** \endcond */
+
+
+#endif /* DART_IF_DART_UTIL_H_ */
