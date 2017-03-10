105e605ec57a0cd8618f9a46d96514f7685286be
Joseph Schuchart <joseph.schuchart@hlrs.de>
2017-03-10 12:27:51 +0100

    Add documentation for macros in dart_util.h

--- dart-if/include/dash/dart/if/dart_util.h
+++ dart-if/include/dash/dart/if/dart_util.h
@@ -7,15 +7,32 @@
 #ifndef DART_UTIL_H_
 #define DART_UTIL_H_
 
+/** \cond DART_HIDDEN_SYMBOLS */
+#define DART_INTERFACE_ON
+/** \endcond */
 
+/**
+ * Mark a function as not throwing C++ exceptions.
+ */
 #ifdef __cplusplus
 #define DART_NOTHROW __attribute__((nothrow))
 #else 
 #define DART_NOTHROW
 #endif
 
+/**
+ * Mark a function (or variable) as possibly unused
+ * (to suppress compiler warnings).
+ */
 #define DART_MAYBE_UNUSED __attribute__((unused))
 
+/**
+ * Mark a (public) function as inline (and potentially unused).
+ */
 #define DART_INLINE static inline DART_MAYBE_UNUSED
+
+/** \cond DART_HIDDEN_SYMBOLS */
+#define DART_INTERFACE_OFF
+/** \endcond */
 
 #endif /* DART_UTIL_H_ */
