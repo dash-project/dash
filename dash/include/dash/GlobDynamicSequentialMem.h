dc592f024b1016decf52dddfe53d4a137fc66d12
Stefan Effenberger <pstiefn@gmail.com>
2017-03-13 13:52:10 +0100

    changed seq globmem functionality

--- dash/include/dash/GlobDynamicSequentialMem.h
+++ dash/include/dash/GlobDynamicSequentialMem.h
@@ -14,6 +14,7 @@
 #include <dash/algorithm/MinMax.h>
 #include <dash/algorithm/Copy.h>
 
+#include <dash/allocator/LocalBucketIter.h>
 #include <dash/allocator/internal/GlobDynamicMemTypes.h>
 
 #include <dash/internal/Logging.h>
@@ -40,8 +41,12 @@ private:
 public:
   typedef ContainerType                                      container_type;
   typedef typename ContainerType::value_type                 value_type;
-  typedef typename ContainerType::iterator                   local_iterator;
+  typedef typename ContainerType::difference_type            index_type;
+  typedef LocalBucketIter<value_type, index_type>            local_iterator;
   typedef typename ContainerType::size_type                  size_type;
+  typedef typename local_iterator::bucket_type               bucket_type;
+  // TODO: use std::array instead of list -> change LocalBucketIter
+  typedef typename std::list<bucket_type>                    bucket_list;
 
 public:
   /**
@@ -55,13 +60,28 @@ public:
     size_type   n_local_elem = 0,
     Team      & team         = dash::Team::All())
   : _container(new container_type()),
-    _public_container(_container),
+    _unattached_container(new container_type()),
+    _buckets(),
     _team(&team),
     _teamid(team.dart_id()),
     _nunits(team.size()),
     _myid(team.myid())
   {
-    grow(n_local_elem);
+    _container->reserve(n_local_elem);
+    bucket_type cont_bucket { 
+      0, 
+      _container->data(), 
+      DART_GPTR_NULL,
+      false
+    };
+    bucket_type unattached_cont_bucket {
+      0,
+      _unattached_container->data(),
+      DART_GPTR_NULL,
+      false
+    };
+    _buckets.push_back(cont_bucket);
+    _buckets.push_back(unattached_cont_bucket);
     commit();
   }
 
@@ -82,23 +102,6 @@ public:
    */
   self_t & operator=(const self_t & rhs) = default;
 
-  local_iterator grow(size_type num_elements) {
-    if(num_elements > 0) {
-      _container->reserve(_container->capacity() + num_elements);
-      _lbegin = _container->begin();
-      _lend = _container->end();
-    }
-    // TODO: return end of container or start of newly added capacity?
-    return _lend;
-  }
-
-  void shrink(size_type num_elements) {
-    if(num_elements > 0) {
-      _container->reserve(_container->capacity() - num_elements);
-      _lend = _container->end();
-    }
-  }
-
   void commit()
   {
   }
@@ -112,29 +115,70 @@ public:
   }
 
   void push_back(value_type val) {
-    // if realloc would get triggered, copy container and keep old data so 
-    // other units can still access the memory region
+    // bucket of _container
+    auto it = _buckets.begin();
+    // use _unattached container, if _container is full
+    // we don't want a realloc of _container because this changes the memory
+    // location, which invalidates global pointers of other units
     if(_container->capacity() == _container->size()) {
-      // TODO: new capacity? if capacity is not increased, push-back will
-      // trigger a realloc and we can use the increase algorithm of the 
-      // container, but this implicates another memory copy.
-      container_type new_container(_container->capacity() * 2);
-      new_container = *_container;
-      if(_public_container != _container) {
-        delete _public_container;
-        _public_container = _container;
-      }
-       _container = &new_container;
-       _lbegin = _container->begin();
+      // bucket of _unattached_container
+      ++it;
+      _unattached_container->push_back(val);
+      // adding data might change the memory location
+      it->lptr = _unattached_container->data();
+    } else {
+      _container->push_back(val);
     }
-    _container->push_back(val);
-    _lend = _container->end();
+    ++(it->size);
+    update_lbegin();
+    update_lend();
+  }
+
+private:
+    /**
+   * Native pointer of the initial address of the local memory of
+   * a unit.
+   *
+   */
+  void update_lbegin() noexcept
+  {
+    DASH_LOG_TRACE("GlobDynamicMem.update_lbegin()");
+    local_iterator unit_lbegin(
+             // iteration space
+             _buckets.begin(), _buckets.end(),
+             // position in iteration space
+             0,
+             // bucket at position in iteration space,
+             // offset in bucket
+             _buckets.begin(), 0);
+    DASH_LOG_TRACE("GlobDynamicMem.update_lbegin >", unit_lbegin);
+    _lbegin = unit_lbegin;
+  }
+
+  /**
+   * Update internal native pointer of the final address of the local memory#include <dash/allocator/LocalBucketIter.h>
+   * of a unit.
+   */
+  void update_lend() noexcept
+  {
+    DASH_LOG_TRACE("GlobDynamicMem.update_lend()");
+    local_iterator unit_lend(
+             // iteration space
+             _buckets.begin(), _buckets.end(),
+             // position in iteration space
+             _container->size() + _unattached_container->size(),
+             // bucket at position in iteration space,
+             // offset in bucket
+             _buckets.end(), 0);
+    DASH_LOG_TRACE("GlobDynamicMem.update_lend >", unit_lend);
+    _lend = unit_lend;
   }
 
 private:
 
   container_type *           _container;
-  container_type *           _public_container;
+  container_type *           _unattached_container = nullptr;
+  bucket_list                _buckets;
   Team *                     _team;
   dart_team_t                _teamid;
   size_type                  _nunits = 0;
