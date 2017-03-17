4ab226cd83dfb4ef286ef633d5fea2f0a50f5c27
Joseph Schuchart <joseph.schuchart@hlrs.de>
2017-03-17 09:52:16 +0100

    Add recursive mutex in dart__base__mutex

--- dart-impl/base/include/dash/dart/base/mutex.h
+++ dart-impl/base/include/dash/dart/base/mutex.h
@@ -1,7 +1,9 @@
 #ifndef DASH_DART_BASE_MUTEX__H_
 #define DASH_DART_BASE_MUTEX__H_
 
+#include <dash/dart/base/logging.h>
 #include <dash/dart/if/dart_types.h>
+#include <dash/dart/if/dart_util.h>
 
 #if defined(DART_ENABLE_THREADSUPPORT) && !defined(DART_HAVE_PTHREADS)
 #error "Thread support has been enabled but PTHREADS support is not available!"
@@ -13,10 +15,6 @@
 
 #ifdef DART_HAVE_PTHREADS
 #include <pthread.h>
-#endif
-
-
-#ifdef DART_HAVE_PTHREADS
 #define DART_MUTEX_INITIALIZER { PTHREAD_MUTEX_INITIALIZER }
 #else
 #define DART_MUTEX_INITIALIZER { 0 }
@@ -33,7 +31,7 @@ char __dummy;
 #endif
 } dart_mutex_t;
 
-static inline
+DART_INLINE
 dart_ret_t
 dart__base__mutex_init(dart_mutex_t *mutex)
 {
@@ -45,7 +43,28 @@ dart__base__mutex_init(dart_mutex_t *mutex)
 #endif
 }
 
-static inline
+DART_INLINE
+dart_ret_t
+dart__base__mutex_init_recursive(dart_mutex_t *mutex)
+{
+#ifdef DART_HAVE_PTHREADS
+  pthread_mutexattr_t attr;
+  pthread_mutexattr_init(&attr);
+  int ret = pthread_mutexattr_settype(&attr, PTHREAD_MUTEX_RECURSIVE_NP);
+  if (ret != 0) {
+    DART_LOG_WARN("dart__base__mutex_init_recursive: Failed to initialize "
+                  "recursive pthread mutex! Falling back to fast mutex...");
+    pthread_mutexattr_settype(&attr, PTHREAD_MUTEX_FAST_NP);
+  }
+  pthread_mutex_init(&mutex->mutex, &attr);
+  pthread_mutexattr_destroy(&attr);
+  return DART_OK;
+#else
+  return DART_ERR_INVAL;
+#endif
+}
+
+DART_INLINE
 dart_ret_t
 dart__base__mutex_lock(dart_mutex_t *mutex)
 {
@@ -57,7 +76,7 @@ dart__base__mutex_lock(dart_mutex_t *mutex)
 #endif
 }
 
-static inline
+DART_INLINE
 dart_ret_t
 dart__base__mutex_unlock(dart_mutex_t *mutex)
 {
@@ -69,7 +88,7 @@ dart__base__mutex_unlock(dart_mutex_t *mutex)
 #endif
 }
 
-static inline
+DART_INLINE
 dart_ret_t
 dart__base__mutex_trylock(dart_mutex_t *mutex)
 {
@@ -81,8 +100,7 @@ dart__base__mutex_trylock(dart_mutex_t *mutex)
 #endif
 }
 
-
-static inline
+DART_INLINE
 dart_ret_t
 dart__base__mutex_destroy(dart_mutex_t *mutex)
 {
