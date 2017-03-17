a28ca9eb36cd4941431b70164dc7bf11ef1404cd
Daniel Diefenthaler <diefenthaler@cip.ifi.lmu.de>
2017-03-17 23:41:53 +0100

    Enabled thread support as default option and enabled HDF5 for build.dev

--- build.debug.sh
+++ build.debug.sh
@@ -60,11 +60,11 @@ rm -Rf $BUILD_DIR/*
                         -DENVIRONMENT_TYPE=default \
                         -DINSTALL_PREFIX=$HOME/opt/dash-0.3.0-dev/ \
                         -DDART_IMPLEMENTATIONS=mpi \
-                        -DENABLE_THREADSUPPORT=OFF \
-                        -DENABLE_ASSERTIONS=ON \
-                        -DENABLE_LT_OPTIMIZATION=OFF \
+                        -DENABLE_THREADSUPPORT=ON \
                         -DENABLE_DEV_COMPILER_WARNINGS=ON \
                         -DENABLE_EXT_COMPILER_WARNINGS=OFF \
+                        -DENABLE_LT_OPTIMIZATION=OFF \
+                        -DENABLE_ASSERTIONS=ON \
                         \
                         -DENABLE_SHARED_WINDOWS=ON \
                         -DENABLE_UNIFIED_MEMORY_MODEL=ON \
