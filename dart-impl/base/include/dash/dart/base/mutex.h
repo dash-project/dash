30841da5e755267b34d1d2ed5b5915946a93397f
Joseph Schuchart <joseph.schuchart@hlrs.de>
2017-03-19 20:45:00 +0100

    Fix dart__base__mutex_trylock and add error checking

--- dart-impl/base/include/dash/dart/base/mutex.h
+++ dart-impl/base/include/dash/dart/base/mutex.h
@@ -36,10 +36,13 @@ dart_ret_t
 dart__base__mutex_init(dart_mutex_t *mutex)
 {
 #ifdef DART_HAVE_PTHREADS
-  pthread_mutex_init(&mutex->mutex, NULL);
+  // pthread_mutex_init always succeeds
+  pthread_mutex_init(&mutex->mutex,
+  DART_LOG_TRACE("%s: Initialized fast mutex %p", __FUNCTION__, mutex);
   return DART_OK;
 #else
-  return DART_ERR_INVAL;
+  DART_LOG_INFO("%s: thread-support disabled", __FUNCTION__);
+  return DART_OK;
 #endif
 }
 
@@ -53,14 +56,18 @@ dart__base__mutex_init_recursive(dart_mutex_t *mutex)
   int ret = pthread_mutexattr_settype(&attr, PTHREAD_MUTEX_RECURSIVE_NP);
   if (ret != 0) {
     DART_LOG_WARN("dart__base__mutex_init_recursive: Failed to initialize "
-                  "recursive pthread mutex! Falling back to fast mutex...");
-    pthread_mutexattr_settype(&attr, PTHREAD_MUTEX_FAST_NP);
+                  "recursive pthread mutex! "
+                  "Falling back to standard mutex...");
+    pthread_mutexattr_destroy(&attr);
+    return dart__base__mutex_init(mutex);
   }
   pthread_mutex_init(&mutex->mutex, &attr);
   pthread_mutexattr_destroy(&attr);
+  DART_LOG_TRACE("%s: Initialized recursive mutex %p", __FUNCTION__, mutex);
   return DART_OK;
 #else
-  return DART_ERR_INVAL;
+  DART_LOG_INFO("%s: thread-support disabled", __FUNCTION__);
+  return DART_OK;
 #endif
 }
 
@@ -69,10 +76,14 @@ dart_ret_t
 dart__base__mutex_lock(dart_mutex_t *mutex)
 {
 #ifdef DART_HAVE_PTHREADS
-  pthread_mutex_lock(&mutex->mutex);
+  int ret = pthread_mutex_lock(&mutex->mutex);
+  if (ret != 0) {
+    DART_LOG_TRACE("%s: Failed to lock mutex (%i)", __FUNCTION__, ret);
+    return DART_ERR_OTHER;
+  }
   return DART_OK;
 #else
-  return DART_ERR_INVAL;
+  return DART_OK;
 #endif
 }
 
@@ -81,10 +92,14 @@ dart_ret_t
 dart__base__mutex_unlock(dart_mutex_t *mutex)
 {
 #ifdef DART_HAVE_PTHREADS
-  pthread_mutex_unlock(&mutex->mutex);
+  int ret = pthread_mutex_unlock(&mutex->mutex);
+  if (ret != 0) {
+    DART_LOG_TRACE("%s: Failed to unlock mutex (%i)", __FUNCTION__, ret);
+    return DART_ERR_OTHER;
+  }
   return DART_OK;
 #else
-  return DART_ERR_INVAL;
+  return DART_OK;
 #endif
 }
 
@@ -93,10 +108,12 @@ dart_ret_t
 dart__base__mutex_trylock(dart_mutex_t *mutex)
 {
 #ifdef DART_HAVE_PTHREADS
-  pthread_mutex_trylock(&mutex->mutex);
-  return DART_OK;
+  int ret = pthread_mutex_trylock(&mutex->mutex);
+  DART_LOG_TRACE("dart__base__mutex_trylock: lock %p aqcuired: %s",
+                 (ret == 0) ? "yes" : "no");
+  return (ret == 0) ? DART_OK : DART_PENDING;
 #else
-  return DART_ERR_INVAL;
+  return DART_OK;
 #endif
 }
 
@@ -105,10 +122,14 @@ dart_ret_t
 dart__base__mutex_destroy(dart_mutex_t *mutex)
 {
 #ifdef DART_HAVE_PTHREADS
-  pthread_mutex_destroy(&mutex->mutex);
+  int ret = pthread_mutex_destroy(&mutex->mutex);
+  if (ret != 0) {
+    DART_LOG_TRACE("%s: Failed to destroy mutex (%i)", __FUNCTION__, ret);
+    return DART_ERR_OTHER;
+  }
   return DART_OK;
 #else
-  return DART_ERR_INVAL;
+  return DART_OK;
 #endif
 }
 
