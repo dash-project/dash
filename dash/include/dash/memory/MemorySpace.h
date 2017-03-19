7d1de1703d9760b14f4293d5e667f5fa2fe6efbe
Tobias Fuchs <fuchst@nm.ifi.lmu.de>
2017-03-19 04:40:59 +0100

    STL-compliant definition of MemorySpace, added documentation and notes

--- dash/include/dash/memory/MemorySpace.h
+++ dash/include/dash/memory/MemorySpace.h
@@ -2,9 +2,14 @@
 #define DASH__MEMORY__MEMORY_SPACE_H__INCLUDED
 
 #include <memory>
-
+#include <cstddef>
 
 namespace dash {
+
+template<
+  typename ElementType,
+  class    MemorySpace >
+class GlobPtr;
 
 /**
  * Pointer types depend on a memory space.
@@ -52,7 +57,7 @@ struct pointer_traits : public std::pointer_traits<Pointer>
  *      pointer;
  *
  *      typedef typename
- *        dash::pointer_traits<pointer>::const_pointer<pointer>::type
+ *        dash::pointer_traits<pointer>::const_pointer<T>::type
  *      const_pointer;
  *
  *      // ...
@@ -61,12 +66,91 @@ struct pointer_traits : public std::pointer_traits<Pointer>
  */
 };
 
-template <
-  class MSpaceCategory
+struct memory_space_global_domain_tag { };
+struct memory_space_local_domain_tag  { };
+
+template <class MemSpace>
+struct memory_space_traits
+{
+  using memory_space_domain_category
+          = typename MemSpace::memory_domain_space_category;
+
+  /**
+   * Whether the memory space type is specified for global address space.
+   */
+  using is_global             = std::integral_constant<
+                                  bool,
+                                  std::is_same<
+                                    memory_space_domain_category,
+                                    memory_space_global_domain_tag
+                                  >::value >;
+  /**
+   * Whether the memory space type is specified for local address space.
+   * As arbitrary address space domains can be defined, this trait is not
+   * equivalent to `!is_global`.
+   */
+  using is_local              = std::integral_constant<
+                                  bool,
+                                  std::is_same<
+                                    memory_space_domain_category,
+                                    memory_space_local_domain_tag
+                                  >::value >;
+  using void_pointer          = typename std::conditional<
+                                  is_global::value,
+                                  dash::GlobPtr<void, MemSpace>,
+                                  void *
+                                >::type;
+};
+
+/**
+ * The \c MemorySpace concept follows the STL \c std::pmr::memory_resource.
+ * 
+ * Unlinke shared memory systems where process memory and virtual address
+ * translation is realized as system functions and hardware components,
+ * PGAS memory resources are also responsible for maintaining and propagating
+ * the size and structure of their underlying local memory ranges.
+ *
+ * Consequently, the memory resource concept is extended by methods and
+ * type definitions that are required to maintain global pointer arithmetics
+ * and named Memory Space in DASH.
+ *
+ * STL Reference of \c std::pmr::memory_resource:
+ * http://en.cppreference.com/w/cpp/memory/memory_resource
+ *
+ * \note
+ *
+ * Defining \c MemorySpace as class template seems to contradict the
+ * intention to use it as polymorphic base class as subclass types of
+ * \c MemorySpace<T> and \c MemorySpace<U> have incompatible polymorphic
+ * base classes.
+ * This looks like the kind of unintentional type incompatibility that lead
+ * to the introduction of polymorphic allocators.
+ * However, the template parameter specifies the memory space domain and
+ * two types of \c MemorySpace are in fact incompatible if they do not refer
+ * to the same domain, like global and local address space.
+ *
+ * \todo
+ *
+ * Clarify:
+ * The STL memory_resource concept assumes that any type can be
+ * allocated with its size specified in number of bytes so untyped
+ * allocation with void pointers is practicable.
+ * However, the DART allocation routines are typed for correctness
+ * considerations and to optimize communication.
+ * If the memory space concept complies to the STL memory_resource
+ * interface, only DART_TYPE_BYTE instead of the actual value type
+ * is available. This would therefore harm stability and performance.
+ */
+template <class MemSpaceDomainCategory>
 class MemorySpace
 {
-   using self_t  = MemorySpace<MSpaceCategory>;
+   using self_t = MemorySpace;
  public:
+   using index_type = dash::default_index_t;
+   using size_type  = dash::default_size_t;
+
+   using memory_space_domain_category = MemSpaceDomainCategory;
+
    // Resolve void pointer type for this MemorySpace, typically
    // `GlobPtr<void>` for global and `void *` for local memory.
    // Allocators use rebind to obtain a fully specified type like
@@ -75,16 +159,66 @@ class MemorySpace
    using void_pointer
            = typename dash::memory_space_traits<self_t>::void_pointer;
 
+ protected:
+   virtual ~MemorySpace();
+
+   virtual void_pointer do_allocate(
+                          std::size_t bytes,
+                          std::size_t alignment) = 0;
+
+   virtual void         do_deallocate(
+                          void_pointer p,
+                          std::size_t  bytes,
+                          std::size_t  alignment) = 0;
+
+   /**
+    * \note
+    * The most-derived type of other may not match the most derived
+    * type of *this.
+    * A derived class implementation therefore must typically check
+    * whether the most derived types of *this and other match using
+    * dynamic_cast, and immediately return false if the cast fails.
+    *
+    * STL Reference:
+    * http://cppreference.com/w/cpp/experimental/memory_resource/do_is_equal
+    */
+   virtual bool         do_is_equal(
+                          const MemorySpace & other) const noexcept = 0;
+
+ public:
    void_pointer allocate(
-      size_t      bytes,
-      std::size_t alignment = alignof(std::max_align_t));
-}
+                  std::size_t bytes,
+                  std::size_t alignment = alignof(std::max_align_t));
 
+   void         deallocate(
+                  void_pointer p,
+                  std::size_t  bytes,
+                  std::size_t  alignment = alignof(std::max_align_t));
 
-struct memory_space_traits
-{
+   /**
+    * Two memory spaces compare equal if and only if memory allocated
+    * from one memory_resource can be deallocated from the other and
+    * vice versa.
+    */
+   bool         is_equal(
+                  const MemorySpace & other) const noexcept;
 
+   size_type    size() const;
+
+   size_type    local_size(
+                  dash::team_unit_t unit) const;
 };
+
+template <class MemSpaceDomainCategory>
+inline MemorySpace<MemSpaceDomainCategory>::~MemorySpace()
+{ }
+
+template <class MemSpaceDomainCategory>
+inline bool operator==(MemorySpace<MemSpaceDomainCategory> const & lhs,
+                       MemorySpace<MemSpaceDomainCategory> const & rhs)
+{
+  return &lhs == &rhs || lhs.is_equal(rhs);
+}
 
 } // namespace dash
 
