a16e681128ba04f15b2b2cfe7b4252310c7040a1
Joseph Schuchart <joseph.schuchart@hlrs.de>
2017-03-28 10:48:42 +0200

    Add test case to stress-test dart_lock

--- dash/test/dart/DARTLockTest.cc
+++ dash/test/dart/DARTLockTest.cc
@@ -13,17 +13,42 @@ TEST_F(DARTLockTest, LockUnlock) {
     shared.set(0);
   }
 
-  dart_team_lock_init(DART_TEAM_ALL, &lock);
+  ASSERT_EQ_U(
+    DART_OK,
+    dart_team_lock_init(DART_TEAM_ALL, &lock));
 
+  dash::barrier();
   for (int i = 0; i < num_iterations; ++i) {
     dart_lock_acquire(lock);
     shared.set(shared.get() + 1);
     dart_lock_release(lock);
   }
-
   dash::barrier();
 
   ASSERT_EQ_U(num_iterations * dash::size(), static_cast<value_t>(shared.get()));
+
+  ASSERT_EQ_U(
+    DART_OK,
+    dart_team_lock_free(DART_TEAM_ALL, &lock));
+
+}
+
+
+TEST_F(DARTLockTest, LockUnlockDoNothing) {
+  using value_t = int;
+  constexpr int num_iterations = 100;
+  dart_lock_t lock;
+
+  ASSERT_EQ_U(
+    DART_OK,
+    dart_team_lock_init(DART_TEAM_ALL, &lock));
+
+  dash::barrier();
+  for (int i = 0; i < num_iterations; ++i) {
+    dart_lock_acquire(lock);
+    dart_lock_release(lock);
+  }
+  dash::barrier();
 
   dart_team_lock_free(DART_TEAM_ALL, &lock);
 
