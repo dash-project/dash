35ca1c50e570dcceaa133b0e13b860a7241300f0
Joseph Schuchart <joseph.schuchart@hlrs.de>
2017-06-27 12:15:46 +0200

    Add test for dash::GlobRef

---
+++ dash/test/types/GlobRefTest.cc
@@ -0,0 +1,45 @@
+
+#include "../TestBase.h"
+#include "../TestLogHelpers.h"
+#include "GlobRefTest.h"
+
+#include <dash/Array.h>
+#include <dash/algorithm/Fill.h>
+#include <dash/algorithm/Copy.h>
+
+
+TEST_F(GlobRefTest, ArithmeticOps)
+{
+  using value_t = int;
+  dash::Array<value_t> arr(dash::size());
+  int neighbor = (dash::myid() + 1) % dash::size();
+  dash::GlobRef<value_t> gref = arr[neighbor];
+
+  // assignment
+  gref = 0;
+  ASSERT_EQ_U(gref, 0);
+
+  // prefix increment
+  ++gref;
+  ASSERT_EQ_U(gref, 1);
+
+  ++(++gref);
+  ASSERT_EQ_U(gref, 3);
+
+  // prefix decrement
+  --(--gref);
+  ASSERT_EQ_U(gref, 1);
+
+  // unary operations
+  gref *= 2;
+  ASSERT_EQ_U(gref, 2);
+
+  gref /= 2;
+  ASSERT_EQ_U(gref, 1);
+
+  gref += 1;
+  ASSERT_EQ_U(gref, 2);
+
+  gref -= 1;
+  ASSERT_EQ_U(gref, 1);
+}
