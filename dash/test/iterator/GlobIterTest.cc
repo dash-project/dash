bf775f0b5dea75cfb298f2daf5ad1985e4324bc7
Felix Moessbauer <felix.moessbauer@campus.lmu.de>
2017-07-28 19:00:36 +0200

    added sorting testcase

--- dash/test/iterator/GlobIterTest.cc
+++ dash/test/iterator/GlobIterTest.cc
@@ -24,6 +24,17 @@ TEST_F(GlobIterTest, Swap) {
 }
 
 TEST_F(GlobIterTest, Sort) {
+  // do not use that in production as this is inefficient as hell!
+  dash::Array<int> arr(dash::size());
+  // init array in reverse order [n-1,n-2,...,0]
+  int locval = dash::size()-1 - dash::myid();
+  arr.local[0] = locval;
+  arr.barrier();
 
+  std::sort(arr.begin(), arr.end());
+  arr.barrier();
+
+  locval = arr.local[0];
+  ASSERT_EQ_U(locval, static_cast<int>(dash::myid()));
 }
 
