c166b52ef0a2ce9a5a21316e845a994759177418
Joseph Schuchart <joseph.schuchart@hlrs.de>
2017-08-09 17:02:23 +0200

    Generate and install dash-mpicxx and dash-shmemcxx

---
+++ CMakeExt/GenerateDASHCXX.cmake
@@ -0,0 +1,61 @@
+# Generate and install DASH compiler wrapper
+
+
+if (";${DART_IMPLEMENTATIONS_LIST};" MATCHES ";mpi;")
+  set(DASHCC ${MPI_CXX_COMPILER})
+  set(DART_IMPLEMENTATION "mpi")
+  configure_file(
+    ${CMAKE_SOURCE_DIR}/dash/scripts/dashcc/dashcxx.in
+    ${CMAKE_BINARY_DIR}/bin/dash-mpicxx
+    @ONLY)
+
+  install(
+    FILES ${CMAKE_BINARY_DIR}/bin/dash-mpicxx 
+    DESTINATION ${CMAKE_INSTALL_PREFIX}/bin
+    PERMISSIONS OWNER_WRITE OWNER_READ GROUP_READ WORLD_READ 
+                OWNER_EXECUTE GROUP_EXECUTE WORLD_EXECUTE)
+
+  install(CODE "execute_process( \
+    COMMAND ${CMAKE_COMMAND} -E create_symlink \
+    dash-mpicxx \
+    ${CMAKE_INSTALL_PREFIX}/bin/dash-mpiCC   \
+    )"
+  )
+
+  install(CODE "execute_process( \
+    COMMAND ${CMAKE_COMMAND} -E create_symlink \
+    dash-mpicxx \
+    ${CMAKE_INSTALL_PREFIX}/bin/dash-mpic++   \
+    )"
+  )
+endif()
+
+
+if (";${DART_IMPLEMENTATIONS_LIST};" MATCHES ";shmem;")
+  set(DASHCC ${CMAKE_CXX_COMPILER})
+  set(DART_IMPLEMENTATION "shmem")
+  configure_file(
+    ${CMAKE_SOURCE_DIR}/dash/scripts/dashcc/dashcxx.in
+    ${CMAKE_BINARY_DIR}/bin/dash-shmemcxx
+    @ONLY)
+
+  install(
+    FILES ${CMAKE_BINARY_DIR}/bin/dash-shmemcxx
+    DESTINATION ${CMAKE_INSTALL_PREFIX}/bin
+    PERMISSIONS OWNER_WRITE OWNER_READ GROUP_READ WORLD_READ 
+                OWNER_EXECUTE GROUP_EXECUTE WORLD_EXECUTE)
+
+  install(CODE "execute_process( \
+    COMMAND ${CMAKE_COMMAND} -E create_symlink \
+    dash-shmemcxx \
+    ${CMAKE_INSTALL_PREFIX}/bin/dash-shmemCC   \
+    )"
+  )
+
+  install(CODE "execute_process( \
+    COMMAND ${CMAKE_COMMAND} -E create_symlink \
+    dash-shmemcxx \
+    ${CMAKE_INSTALL_PREFIX}/bin/dash-shmemc++   \
+    )"
+  )
+endif()
