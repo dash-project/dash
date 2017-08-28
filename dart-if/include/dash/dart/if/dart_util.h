cc45b5f2cd5a64f8851c34d7dfbd7a1a6d99c29a
Joseph Schuchart <joseph.schuchart@hlrs.de>
2017-08-28 16:33:03 +0200

    Add macro DART_NORETURN, e.g., for dart_abort

--- dart-if/include/dash/dart/if/dart_util.h
+++ dart-if/include/dash/dart/if/dart_util.h
@@ -1,6 +1,6 @@
 /*
  * \file dart_util.h
- * 
+ *
  * utility macros for the DART interface
  */
 
@@ -16,7 +16,7 @@
  */
 #ifdef __cplusplus
 #define DART_NOTHROW __attribute__((nothrow))
-#else 
+#else
 #define DART_NOTHROW
 #endif
 
@@ -30,6 +30,11 @@
  * Mark a (public) function as inline (and potentially unused).
  */
 #define DART_INLINE static inline DART_MAYBE_UNUSED
+
+/**
+ * Mark a function as not returning to the caller.
+ */
+#define DART_NORETURN __attribute__((noreturn))
 
 /** \cond DART_HIDDEN_SYMBOLS */
 #define DART_INTERFACE_OFF
