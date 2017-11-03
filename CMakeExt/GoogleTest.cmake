978730d3afde59d54c019f8f67fd373d5f37ea81
Felix Moessbauer <felix.moessbauer@gmail.com>
2017-11-03 16:23:17 +0100

    gtest: disable ninja support for cmake < 3.2.0

--- CMakeExt/GoogleTest.cmake
+++ CMakeExt/GoogleTest.cmake
@@ -46,21 +46,39 @@ else()
   set(GTEST_LIBRARY  "${GTEST_LOCATION}/${LIBPREFIX}gtest${LIBSUFFIX}")
   set(GTEST_MAINLIB  "${GTEST_LOCATION}/${LIBPREFIX}gtest_main${LIBSUFFIX}")
 
-  ExternalProject_Add(
-    GTestExternal
-    GIT_REPOSITORY https://github.com/google/googletest.git
-    GIT_TAG master
-    TIMEOUT 10
-    PREFIX "${GTEST_PREFIX}"
-    CMAKE_ARGS "-DCMAKE_C_COMPILER:string=${CMAKE_C_COMPILER};-DCMAKE_CXX_COMPILER:string=${CMAKE_CXX_COMPILER}"
-    INSTALL_COMMAND ""
-    # Necessary for ninja build
-    BUILD_BYPRODUCTS ${GTEST_LIBRARY}
-    # Wrap download, configure and build steps in a script to log output
-    LOG_DOWNLOAD ON
-    LOG_CONFIGURE ON
-    LOG_BUILD ON
-  )
+
+  # BUILD_BYPRODUCTS not avalable in CMAKE < 3.2.0
+  if("${CMAKE_VERSION}" VERSION_LESS 3.2.0)
+    ExternalProject_Add(
+      GTestExternal
+      GIT_REPOSITORY https://github.com/google/googletest.git
+      GIT_TAG master
+      TIMEOUT 10
+      PREFIX "${GTEST_PREFIX}"
+      CMAKE_ARGS "-DCMAKE_C_COMPILER:string=${CMAKE_C_COMPILER};-DCMAKE_CXX_COMPILER:string=${CMAKE_CXX_COMPILER}"
+      INSTALL_COMMAND ""
+      # Wrap download, configure and build steps in a script to log output
+      LOG_DOWNLOAD ON
+      LOG_CONFIGURE ON
+      LOG_BUILD ON
+    )
+  else()
+    ExternalProject_Add(
+      GTestExternal
+      GIT_REPOSITORY https://github.com/google/googletest.git
+      GIT_TAG master
+      TIMEOUT 10
+      PREFIX "${GTEST_PREFIX}"
+      CMAKE_ARGS "-DCMAKE_C_COMPILER:string=${CMAKE_C_COMPILER};-DCMAKE_CXX_COMPILER:string=${CMAKE_CXX_COMPILER}"
+      INSTALL_COMMAND ""
+      # Necessary for ninja build
+      BUILD_BYPRODUCTS ${GTEST_LIBRARY}
+      # Wrap download, configure and build steps in a script to log output
+      LOG_DOWNLOAD ON
+      LOG_CONFIGURE ON
+      LOG_BUILD ON
+    )
+endif()
 
   add_library(GTest IMPORTED STATIC GLOBAL)
   set_target_properties(
