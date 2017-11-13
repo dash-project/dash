93509c74a24d30fdf20f3b61f36d1a72eff7fd37
Joseph Schuchart <joseph.schuchart@hlrs.de>
2017-11-13 12:02:47 +0900

    Extend tests for C++ task interface

--- dash/test/tasks/TaskingTest.cc
+++ dash/test/tasks/TaskingTest.cc
@@ -2,6 +2,7 @@
 
 #include <dash/tasks/Tasks.h>
 #include <dash/Array.h>
+#include <dash/Matrix.h>
 #include <dash/algorithm/Fill.h>
 
 TEST_F(TaskingTest, SimpleTasks)
@@ -95,17 +96,16 @@ TEST_F(TaskingTest, LocalDeps)
 }
 
 
-TEST_F(TaskingTest, RemoteCentralDeps)
+TEST_F(TaskingTest, RemoteDepsCentral)
 {
-  int i;
   int num_iter = 10;
   dash::Array<int> array(dash::size());
   array.local[0] = 0;
   dash::barrier();
 
-  for (i = 1; i <= num_iter; i++) {
+  for (int i = 0; i < num_iter; i++) {
     // have all units except for 0 skip the first round
-    if (dash::myid() == 0 || i > 1) {
+    if (dash::myid() == 0 || i > 0) {
       dash::tasks::async(
         [&array, i](){
           int val = array[0];
@@ -116,9 +116,137 @@ TEST_F(TaskingTest, RemoteCentralDeps)
         dash::tasks::out(array[dash::myid()])
       );
     }
+    dash::tasks::async_barrier();
   }
 
   dash::tasks::complete();
 
   ASSERT_EQ_U(num_iter, array.local[0]);
+}
+
+
+TEST_F(TaskingTest, RemoteDepsSweep)
+{
+  int num_iter = 10;
+  int num_elems_per_unit = 10;
+  dash::Matrix<int, 2> matrix(dash::size(), num_elems_per_unit);
+  dash::fill(matrix.begin(), matrix.end(), 0);
+  dash::barrier();
+
+  auto myid     = dash::myid();
+  auto neighbor = (myid + dash::size() - 1) % dash::size();
+
+  /**
+   * multi-iteration wrap-around sweep from top to bottom
+   *
+   * Input:
+   *  0  0  0  0  0  0  0  0  0  0
+   *  0  0  0  0  0  0  0  0  0  0
+   *  0  0  0  0  0  0  0  0  0  0
+   *
+   * After first iteration:
+   *  1  1  1  1  1  1  1  1  1  1
+   *  2  2  2  2  2  2  2  2  2  2
+   *  3  3  3  3  3  3  3  3  3  3
+   *  4  4  4  4  4  4  4  4  4  4
+   *
+   * After second iteration: (notice the wrap-around)
+   *  5  5  5  5  5  5  5  5  5  5
+   *  6  6  6  6  6  6  6  6  6  6
+   *  7  7  7  7  7  7  7  7  7  7
+   *  8  8  8  8  8  8  8  8  8  8
+   */
+  for (int i = 0; i < num_iter; i++) {
+    for (size_t unit = 0; unit < dash::size(); ++unit) {
+      if (unit == myid) {
+        for (int j = 0; j < num_elems_per_unit; j++) {
+          dash::tasks::async(
+            [=, &matrix](){
+              int val = matrix[neighbor][j];
+              ASSERT_EQ_U(i*dash::size() + myid, val);
+              matrix[myid][j] = val + 1;
+            },
+            dash::tasks::in (matrix[neighbor][j]),
+            dash::tasks::out(matrix[    myid][j])
+          );
+        }
+      }
+      // traditionally, this would be a dash::barrier() to wait for
+      // neighboring unit to complete
+      dash::tasks::async_barrier();
+    }
+  }
+
+  dash::tasks::complete();
+
+  for (int i = 0; i < num_elems_per_unit; ++i) {
+    ASSERT_EQ_U((num_iter-1)*dash::size() + myid + 1, matrix.local[0][i]);
+  }
+}
+
+
+TEST_F(TaskingTest, RemoteDepsDoubleBufferStencil)
+{
+  int num_iter = 10;
+  int num_elems_per_unit = 10;
+  using MatrixT = dash::Matrix<int, 2>;
+  auto myid     = dash::myid();
+  auto up       = (myid + dash::size() - 1) % dash::size();
+  auto down       = (myid + 1) % dash::size();
+
+  MatrixT matrix1(dash::size(), num_elems_per_unit);
+  MatrixT matrix2(dash::size(), num_elems_per_unit);
+  dash::fill(matrix1.begin(), matrix1.end(), myid);
+  dash::fill(matrix2.begin(), matrix2.end(), myid);
+  dash::barrier();
+
+  for (int i = 0; i < num_iter; ++i) {
+    // double buffer swap
+    MatrixT& oldmat = (i%2) ? matrix1 : matrix2;
+    MatrixT& newmat = (i%2) ? matrix2 : matrix1;
+    for (int j = 0; j < num_elems_per_unit; ++j) {
+      dash::tasks::async(
+        [=, &oldmat, &newmat](){
+          // check correct values in 5pt stencil
+
+          // center
+          ASSERT_EQ_U(i + myid, static_cast<int>(oldmat(myid, j)));
+          // up
+          if (myid > 0) {
+            ASSERT_EQ_U(i + up, static_cast<int>(oldmat(up, j)));
+          }
+          // down
+          if (myid < dash::size()-1) {
+            ASSERT_EQ_U(i + down, static_cast<int>(oldmat(down, j)));
+          }
+          // left
+          if (j > 0) {
+            ASSERT_EQ_U(i + myid, static_cast<int>(oldmat(myid, j)));
+          }
+          // right
+          if (j < num_elems_per_unit-1) {
+            ASSERT_EQ_U(i + myid, static_cast<int>(oldmat(myid, j)));
+          }
+
+          // update value (ignoring stencil values for simplicity)
+          auto value = oldmat(myid, j) + 1;
+          ASSERT_EQ_U(i + myid + 1, value);
+          newmat(myid, j) = value;
+        },
+        dash::tasks::in(oldmat(myid, j)),
+        dash::tasks::in(oldmat(  up, j)),
+        dash::tasks::in(oldmat(down, j)),
+        (j > 0) ? dash::tasks::in(oldmat(myid, j-1)) : dash::tasks::none(),
+        (j < num_elems_per_unit-1) ? dash::tasks::in(oldmat(myid, j+1))
+                                   : dash::tasks::none(),
+        dash::tasks::out(newmat(myid, j))
+      );
+    }
+    dash::tasks::async_barrier();
+  }
+  dash::tasks::complete();
+
+  for (int i = 0; i < num_elems_per_unit; ++i) {
+    ASSERT_EQ_U(num_iter+myid, matrix2.local(0, i));
+  }
 }
