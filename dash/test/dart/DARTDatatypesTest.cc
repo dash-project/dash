1a0fc192251574fb485ef4d7bec2955e96902451
Joseph Schuchart <joseph.schuchart@hlrs.de>
2017-11-18 12:22:18 +0900

    Add IndexedGetSimple test case

--- dash/test/dart/DARTDatatypesTest.cc
+++ dash/test/dart/DARTDatatypesTest.cc
@@ -196,3 +196,78 @@ TEST_F(DARTDatatypesTest, BlockedStridedToStrided) {
   gptr.unitid = 0;
   dart_team_memfree(gptr);
 }
+
+
+TEST_F(DARTDatatypesTest, IndexedGetSimple) {
+
+  constexpr size_t num_elem_per_unit = 120;
+  constexpr size_t num_blocks        = 5;
+
+  std::vector<size_t> blocklens(num_blocks);
+  std::vector<size_t> offsets(num_blocks);
+
+  // set up offsets and block lengths
+  size_t num_elems = 0;
+  for (int i = 0; i < num_blocks; ++i) {
+    blocklens[i] = (i+1);
+    offsets[i]   = (i*10);
+    num_elems   += blocklens[i];
+  }
+
+  dart_gptr_t gptr;
+  int *local_ptr;
+  dart_team_memalloc_aligned(
+    DART_TEAM_ALL, num_elem_per_unit, DART_TYPE_INT, &gptr);
+  gptr.unitid = dash::myid();
+  dart_gptr_getaddr(gptr, (void**)&local_ptr);
+  for (int i = 0; i < num_elem_per_unit; ++i) {
+    local_ptr[i] = i;
+  }
+
+  dart_datatype_t new_type;
+  dart_type_create_indexed(DART_TYPE_INT, num_blocks, blocklens.data(),
+                           offsets.data(), &new_type);
+
+  dash::barrier();
+
+  int *buf = new int[num_elem_per_unit];
+  memset(buf, 0, sizeof(int)*num_elem_per_unit);
+
+  // indexed-to-contig
+  dart_get_blocking(buf, gptr, num_elems, new_type, DART_TYPE_INT);
+
+  size_t idx = 0;
+  for (size_t i = 0; i < num_blocks; ++i) {
+    for (size_t j = 0; j < blocklens[i]; ++j) {
+      ASSERT_EQ_U(local_ptr[offsets[i] + j], buf[idx]);
+      ++idx;
+    }
+  }
+
+  // check we haven't copied more elements than requested
+  for (size_t i = idx; i < num_elem_per_unit; ++i) {
+    ASSERT_EQ_U(0, buf[i]);
+  }
+
+
+  // contig-to-indexed
+  memset(buf, 0, sizeof(int)*num_elem_per_unit);
+
+  // indexed-to-contig
+  dart_get_blocking(buf, gptr, num_elems, DART_TYPE_INT, new_type);
+
+  idx = 0;
+  for (size_t i = 0; i < num_blocks; ++i) {
+    for (size_t j = 0; j < blocklens[i]; ++j) {
+      ASSERT_EQ_U(local_ptr[idx], buf[offsets[i] + j]);
+      ++idx;
+    }
+  }
+
+  dart_type_destroy(&new_type);
+
+  delete[] buf;
+  // clean-up
+  gptr.unitid = 0;
+  dart_team_memfree(gptr);
+}
