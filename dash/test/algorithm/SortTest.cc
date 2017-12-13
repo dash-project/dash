879d0f241f3daaadf4a4a336be02b9f085a301b7
Roger Kowalewski <roger.kowalewski@gmail.com>
2017-12-13 23:36:38 +0100

    enhance unit test for dash::sort

--- dash/test/algorithm/SortTest.cc
+++ dash/test/algorithm/SortTest.cc
@@ -8,8 +8,8 @@
 
 TEST_F(SortTest, ArraySort)
 {
-  typedef uint32_t                                   Element_t;
-  typedef dash::Array<Element_t>                     Array_t;
+  typedef uint32_t               Element_t;
+  typedef dash::Array<Element_t> Array_t;
 
   static std::uniform_int_distribution<Element_t> distribution(0, 100);
   static std::random_device                       rd;
@@ -27,9 +27,16 @@ TEST_F(SortTest, ArraySort)
   // Wait for all units
   array.barrier();
 
+  auto const true_sum = dash::accumulate(array.begin(), array.end(), 0);
+
   dash::sort(array.begin(), array.end());
 
+  auto const actual_sum = dash::accumulate(array.begin(), array.end(), 0);
+
   if (dash::myid() == 0) {
+
+    EXPECT_EQ_U(true_sum, actual_sum);
+
     for (auto it = array.begin() + 1; it < array.end(); ++it) {
       auto const a = static_cast<const Element_t>(*(it - 1));
       auto const b = static_cast<const Element_t>(*it);
@@ -39,4 +46,4 @@ TEST_F(SortTest, ArraySort)
   }
 }
 
-//TODO: add additional unit tests with various pattern types and containers
+// TODO: add additional unit tests with various pattern types and containers
