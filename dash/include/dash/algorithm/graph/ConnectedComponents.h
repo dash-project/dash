fa6a97f1666f88d729af836ca3378190d928c616
Stefan Effenberger <pstiefn@gmail.com>
2018-02-14 15:55:04 +0100

    reduced algorithm complexity

--- dash/include/dash/algorithm/graph/ConnectedComponents.h
+++ dash/include/dash/algorithm/graph/ConnectedComponents.h
@@ -2,6 +2,7 @@
 #define DASH__ALGORITHM____GRAPH__CONNECTED_COMPONENTS_H__
 
 #include <dash/Graph.h>
+#include <dash/util/Trace.h>
 
 namespace dash {
 
@@ -14,8 +15,10 @@ template<typename GraphType>
 std::vector<int> cc_get_data(
     matrix_t & indices, 
     matrix_t & permutations, 
-    GraphType & graph) 
+    GraphType & graph,
+    dash::util::Trace & trace) 
 {
+  trace.enter_state("send indices");
   // exchange indices to get
   std::vector<std::size_t> sizes_send(indices.size());
   std::vector<std::size_t> displs_send(indices.size());
@@ -47,17 +50,23 @@ std::vector<int> cc_get_data(
         displs_recv.data(), graph.team().dart_id());
   }
 
+  trace.exit_state("send indices");
   // exchange data
+  trace.enter_state("get components");
   for(auto & index : indices_recv) {
     // TODO: optimize cache performance
-    index = graph[graph.vertices().begin() + index].attributes().comp;
+    index = graph[graph.vertices().lbegin() + index].attributes().comp;
   }
+  trace.exit_state("get components");
+  trace.enter_state("send components");
   if(total_send > 0 || total_recv > 0) {
     dart_alltoallv(indices_recv.data(), sizes_recv.data(), displs_recv.data(),
         DART_TYPE_INT, indices_send.data(), sizes_send.data(), 
         displs_send.data(), graph.team().dart_id());
   }
+  trace.exit_state("send components");
 
+  trace.enter_state("restore order");
   // restore original order
   // TODO: use more sophisticated ordering mechanism
   std::vector<int> output(indices_send.size());
@@ -68,11 +77,14 @@ std::vector<int> cc_get_data(
       ++index;
     }
   }
+  trace.exit_state("restore order");
   return output;
 }
 
 template<typename GraphType>
-void cc_set_data(matrix_pair_t & data_pairs, GraphType & graph) {
+void cc_set_data(matrix_pair_t & data_pairs, GraphType & graph, 
+    dash::util::Trace & trace) {
+  trace.enter_state("send pairs");
   std::vector<std::size_t> sizes_send(data_pairs.size());
   std::vector<std::size_t> displs_send(data_pairs.size());
   std::vector<std::pair<int, int>> pairs_send;
@@ -102,10 +114,13 @@ void cc_set_data(matrix_pair_t & data_pairs, GraphType & graph) {
       DART_TYPE_BYTE, pairs_recv.data(), sizes_recv.data(), 
       displs_recv.data(), graph.team().dart_id());
   }
+  trace.exit_state("send pairs");
+  trace.enter_state("set components");
   for(auto & pair : pairs_recv) {
     typename GraphType::vertex_properties_type prop { pair.second };
-    graph[graph.vertices().begin() + pair.first].set_attributes(prop);
+    graph[graph.vertices().lbegin() + pair.first].set_attributes(prop);
   }
+  trace.exit_state("set components");
 }
 
 } // namespace internal
@@ -123,8 +138,10 @@ void cc_set_data(matrix_pair_t & data_pairs, GraphType & graph) {
  */
 template<typename GraphType>
 void connected_components(GraphType & g) {
+  dash::util::Trace trace("ConnectedComponents");
   // set component to global index in iteration space
   // TODO: find faster method for this
+  trace.enter_state("vertex setup");
   int i = 0;
   for(auto it = g.vertices().begin(); it != g.vertices().end(); ++it) {
     if(it.is_local()) {
@@ -133,13 +150,17 @@ void connected_components(GraphType & g) {
     }
     ++i;
   }
+  trace.exit_state("vertex setup");
 
+  trace.enter_state("barrier");
   dash::barrier();
+  trace.exit_state("barrier");
 
   while(1) {
     int gr = 0;
     std::vector<int> data;
     {
+      trace.enter_state("compute indices");
       matrix_t indices(g.team().size());
       matrix_t permutations(g.team().size());
       int i = 0;
@@ -147,14 +168,16 @@ void connected_components(GraphType & g) {
         auto trg = g[it].target();
         auto lpos = trg.lpos();
         // TODO: use more sophsticated sorting mechanism
-        indices[lpos.unit].push_back(trg.pos());
+        indices[lpos.unit].push_back(lpos.index);
         permutations[lpos.unit].push_back(i);
         ++i;
       }
-      data = internal::cc_get_data(indices, permutations, g);
+      trace.exit_state("compute indices");
+      data = internal::cc_get_data(indices, permutations, g, trace);
     }
 
     {
+      trace.enter_state("compute pairs");
       matrix_pair_t data_pairs(g.team().size());
       int i = 0;
       for(auto it = g.out_edges().lbegin(); it != g.out_edges().lend(); ++it) {
@@ -164,22 +187,27 @@ void connected_components(GraphType & g) {
         auto trg_comp = data[i];
         if(src_comp < trg_comp) {
           auto trg_comp_it = g.vertices().begin() + trg_comp;
-           data_pairs[trg_comp_it.lpos().unit].push_back(
-               std::make_pair(trg_comp_it.pos(), src_comp)
+          auto lpos = trg_comp_it.lpos();
+           data_pairs[lpos.unit].push_back(
+               std::make_pair(lpos.index, src_comp)
           );
           gr = 1;
         }
         ++i;
       }
-      internal::cc_set_data(data_pairs, g);
+      trace.exit_state("compute pairs");
+      internal::cc_set_data(data_pairs, g, trace);
     }
     int gr_all = 0;
+    trace.enter_state("allreduce data");
     dart_allreduce(&gr, &gr_all, 1, DART_TYPE_INT, DART_OP_SUM, 
         g.team().dart_id());
+    trace.exit_state("allreduce data");
     if(gr_all == 0) break; 
     while(1) {
       int pj = 0;
       {
+        trace.enter_state("compute indices (pj)");
         matrix_t indices(g.team().size());
         matrix_t permutations(g.team().size());
         int i = 0;
@@ -188,13 +216,15 @@ void connected_components(GraphType & g) {
           auto comp = v.attributes().comp;
           auto next_it = g.vertices().begin() + comp;
           auto lpos = next_it.lpos();
-          indices[lpos.unit].push_back(next_it.pos());
+          indices[lpos.unit].push_back(lpos.index);
           permutations[lpos.unit].push_back(i);
           ++i;
         }
-        data = internal::cc_get_data(indices, permutations, g);
+        trace.exit_state("compute indices (pj)");
+        data = internal::cc_get_data(indices, permutations, g, trace);
       }
 
+      trace.enter_state("set data (pj)");
       int i = 0;
       for(auto it = g.vertices().lbegin(); it != g.vertices().lend(); ++it) {
         auto v = g[it];
@@ -207,12 +237,14 @@ void connected_components(GraphType & g) {
         }
         ++i;
       }
+      trace.exit_state("set data (pj)");
       int pj_all = 0;
+      trace.enter_state("allreduce pointerjumping");
       dart_allreduce(&pj, &pj_all, 1, DART_TYPE_INT, DART_OP_SUM, 
           g.team().dart_id());
+      trace.exit_state("allreduce pointerjumping");
       if(pj_all == 0) break; 
     }
-    dash::barrier();
   }
   dash::barrier();
 } 
