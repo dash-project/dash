43ea7a4865bdfd57fe23084958b72659502e1c24
Felix Moessbauer <felix.moessbauer@campus.lmu.de>
2018-04-19 12:51:43 +0200

    added openmpi3 ci container, use up to 4 units

--- dash/scripts/circleci/collect-artifacts.sh
+++ dash/scripts/circleci/collect-artifacts.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+# Copy Build artifacts to tmp directory to collect them later using circleci
 
 [[ "$CIRCLECI" == "true" ]] || { echo "Not running on CircleCI"; exit 1; }
 
@@ -9,10 +10,9 @@ cd build-ci
 
 echo "Collect Logs"
 cp -v --parents -R ./*/*/*/*.log /tmp/build-logs/${CIRCLE_JOB}
+echo "Artifacts copied to /tmp/build-logs/${CIRCLE_JOB}"
 
 echo "Collect Tests"
 cp -v --parents -R ./*/*/*/dash-tests-*.xml /tmp/build-tests/${CIRCLE_JOB}
-
-echo "Artifacts copied to /tmp/build-logs/${CIRCLE_JOB}"
 echo "Testlogs copied to /tmp/build-tests/${CIRCLE_JOB}"
 
