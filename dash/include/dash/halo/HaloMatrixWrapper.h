19780d1c78c68f8cf353d52440a2578845fadc88
Roger Kowalewski <kowalewski@nm.ifi.lmu.de>
2018-04-27 15:10:24 +0200

    remove default value initializer

--- dash/include/dash/halo/HaloMatrixWrapper.h
+++ dash/include/dash/halo/HaloMatrixWrapper.h
@@ -450,7 +450,7 @@ private:
   struct Data {
     const Region_t&                     region;
     std::function<void(dart_handle_t&)> get_halos;
-    dart_handle_t                       handle = DART_HANDLE_NULL;
+    dart_handle_t                       handle;
   };
 
   void update_halo_intern(Data& data) {
