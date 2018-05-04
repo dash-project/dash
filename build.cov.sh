b6bed30b08cde48fef7153e119820403408990f6
Felix Moessbauer <felix.moessbauer@gmail.com>
2018-05-04 08:05:53 +0200

    disabled threadsupport in coverage builds

--- build.cov.sh
+++ build.cov.sh
@@ -58,7 +58,7 @@ rm -Rf $BUILD_DIR/*
                         -DENVIRONMENT_TYPE=default \
                         -DINSTALL_PREFIX=$HOME/opt/dash-0.3.0/ \
                         -DDART_IMPLEMENTATIONS=mpi \
-                        -DENABLE_THREADSUPPORT=ON \
+                        -DENABLE_THREADSUPPORT=OFF \
                         -DENABLE_DEV_COMPILER_WARNINGS=OFF \
                         -DENABLE_EXT_COMPILER_WARNINGS=OFF \
                         -DENABLE_LT_OPTIMIZATION=OFF \
