2416e452a86f437e7ac5aad045081fc5f3d730b3
Roger Kowalewski <roger.kowalewski@gmail.com>
2018-05-06 13:20:12 +0200

    exclude macro from clang-tidy as it causes syntax errors

--- dash/include/dash/algorithm/Sort.h
+++ dash/include/dash/algorithm/Sort.h
@@ -737,10 +737,10 @@ struct closure_traits : closure_traits<decltype(&T::operator())> {
 #define REM_CTOR(...) __VA_ARGS__
 #define SPEC(cv, var, is_var)                                                 \
   template <typename C, typename R, typename... Args>                         \
-  struct closure_traits<R (C::*)(Args... REM_CTOR var) cv> {                  \
+  struct closure_traits<R (C::*)(Args... REM_CTOR var) cv> { /* NOLINT */     \
     using arity       = std::integral_constant<std::size_t, sizeof...(Args)>; \
     using is_variadic = std::integral_constant<bool, is_var>;                 \
-    using is_const    = std::is_const<int cv>;                                \
+    using is_const    = std::is_const<int cv>; /* NOLINT */                   \
                                                                               \
     using result_type = R;                                                    \
                                                                               \
