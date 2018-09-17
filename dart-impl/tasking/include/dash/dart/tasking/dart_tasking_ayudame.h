cd512bb6153dca20620d6ebfed4bc8820a182756
Joseph Schuchart <joseph.schuchart@hlrs.de>
2018-09-17 22:54:49 +0200

    Ayudame: Add functions to translate task pointer into globally unique task pointer

--- dart-impl/tasking/include/dash/dart/tasking/dart_tasking_ayudame.h
+++ dart-impl/tasking/include/dash/dart/tasking/dart_tasking_ayudame.h
@@ -1,8 +1,21 @@
 #ifndef DART__TASKING__INTERNAL_AYURDAME_H__
 #define DART__TASKING__INTERNAL_AYURDAME_H__
 
+#include <ayu_events.h>
 
 #ifdef AYU_UNKNOWN_NTHREADS
+
+static inline
+uint64_t dart__tasking__ayudame_make_globalunique_unit(
+  dart_task_t       *task,
+  dart_global_unit_t unit)
+{
+  uint64_t myid64 = unit.id;
+  uint64_t task64 = (uint64_t)task;
+  uint64_t mask64 = (1UL<<48) - 1;
+
+  return (task64 & mask64) | (myid64 << 48);
+}
 
 static inline
 uint64_t dart__tasking__ayudame_make_globalunique(dart_task_t *task)
@@ -10,16 +23,13 @@ uint64_t dart__tasking__ayudame_make_globalunique(dart_task_t *task)
   dart_global_unit_t myid;
   dart_myid(&myid);
 
-  uint64_t myid64 = myid.id;
-  uint64_t task64 = (uint64_t)task;
-  uint64_t mask64 = (1UL<<48) - 1;
-
-  return (task64 & mask64) | (myid64 << 48);
+  return dart__tasking__ayudame_make_globalunique_unit(task, myid);
 }
 
 #else
 
 #define dart__tasking__ayudame_make_globalunique(__task) ((uint64_t)__task)
+#define dart__tasking__ayudame_make_globalunique_unit(__task, unit) ((uint64_t)__task)
 
 #endif // AYU_UNKNOWN_NTHREADS
 
