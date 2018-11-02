2178c61154626217e56a50a3d8b12e4871b47817
Joseph Schuchart <joseph.schuchart@hlrs.de>
2018-11-02 12:08:10 -0400

    Add overload for dash::Future<void>

--- dash/include/dash/Future.h
+++ dash/include/dash/Future.h
@@ -216,6 +216,197 @@ public:
 
 }; // class Future
 
+
+/**
+ * Overload of \ref dash::Future for operations returning \c void.
+ *
+ */
+template<>
+class Future<void>
+{
+public:
+  typedef Future<void>               self_t;
+
+  /**
+   * Callback function to wait for completion.
+   */
+  typedef std::function<void (void)> get_func_t;
+
+  /**
+   * Callback function to test for completion.
+   */
+  typedef std::function<bool (void)> test_func_t;
+
+  /**
+   * Callback function called upon destruction.
+   */
+  typedef std::function<void (void)> destroy_func_t;
+
+private:
+  /// Function returning the value
+  get_func_t     _get_func;
+  /// Function used to test for the availability of a value
+  test_func_t    _test_func;
+  /// Function called upon destruction of the future
+  destroy_func_t _destroy_func;
+  /// Whether or not the value is available
+  bool           _ready = false;
+
+public:
+  // For ostream output
+  template<typename ResultT_>
+  friend std::ostream & operator<<(
+      std::ostream & os,
+      const Future<ResultT_> & future);
+
+public:
+
+  /**
+   * Default constructor, creates a future that invalid.
+   *
+   * \see dash::Future::valid
+   */
+  Future() noexcept
+  { }
+
+  /**
+   * Create a future using a function that returns the value.
+   *
+   * \param get_func  Function blocking until the operation is complete.
+   */
+  Future(const get_func_t & get_func)
+  : _get_func(get_func)
+  { }
+
+  /**
+   * Create a future using a function that returns the value and a function
+   * to test whether the result value is available.
+   *
+   * \param get_func  Function blocking until the operation is complete.
+   * \param test_func Function returning \c true if the value is available.
+   */
+  Future(
+    const get_func_t     & get_func,
+    const test_func_t    & test_func)
+  : _get_func(get_func),
+    _test_func(test_func)
+  { }
+
+  /**
+   * Create a future using a function to wait for completion and a function
+   * to test for completion, plus a function to be called upon destruction
+   * of the future.
+   *
+   * \param get_func  Function to wait for completion.
+   * \param test_func Function returning \c true if the operation is complete.
+   * \param destroy_func Function called upon destruction of the future.
+   */
+  Future(
+    const get_func_t     & get_func,
+    const test_func_t    & test_func,
+    const destroy_func_t & destroy_func)
+  : _get_func(get_func),
+    _test_func(test_func),
+    _destroy_func(destroy_func)
+  { }
+
+  /**
+   * Copy construction is not permited.
+   */
+  Future(const self_t& other) = delete;
+
+  /**
+   * Default move constructor.
+   */
+  Future(self_t&& other)      = default;
+
+  /**
+   * Destructor. Calls the \c destroy_func passed to the constructor,
+   * if available.
+   */
+  ~Future() {
+    if (_destroy_func) {
+      _destroy_func();
+    }
+  }
+
+  /**
+   * Copy assignment is not permited.
+   */
+  self_t& operator=(const self_t& other) = delete;
+
+  /**
+   * Move assignment is defaulted.
+   */
+  self_t & operator=(self_t&& other)      = default;
+
+
+  /**
+   * Wait for the value to become available. It is safe to call \ref get
+   * after this call returned.
+   */
+  void wait()
+  {
+    DASH_LOG_TRACE_VAR("Future.wait()", _ready);
+    if (_ready) {
+      return;
+    }
+    if (!_get_func) {
+      DASH_LOG_ERROR("Future.wait()", "No function");
+      DASH_THROW(
+        dash::exception::RuntimeError,
+        "Future not initialized with function");
+    }
+    _get_func();
+    _ready = true;
+    DASH_LOG_TRACE_VAR("Future.wait >", _ready);
+  }
+
+  /**
+   * Test whether the operation has completed.
+   *
+   * Blocks if no test-function has been provided.
+   *
+   * \return \c true if the value is available, \c false otherwise.
+   */
+  bool test()
+  {
+    if (!_ready) {
+      if (_test_func) {
+        _ready = _test_func();
+      } else if (_get_func) {
+        _get_func();
+        _ready = true;
+      } else {
+        DASH_THROW(
+          dash::exception::RuntimeError,
+          "Future not initialized with function");
+      }
+    }
+    return _ready;
+  }
+
+  /**
+   * Return after making sure that the operation is completed.
+   */
+  void get()
+  {
+    DASH_LOG_TRACE_VAR("Future.get()", _ready);
+    wait();
+    DASH_LOG_TRACE("Future.get >");
+  }
+
+  /**
+   * Check whether the future is valid, i.e., a function to wait for completion
+   * has been provided.
+   */
+  bool valid() noexcept
+  {
+    return (_get_func != nullptr);
+  }
+
+}; // class Future
+
 template<typename ResultT>
 std::ostream & operator<<(
   std::ostream & os,
