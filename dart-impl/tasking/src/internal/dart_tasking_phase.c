fb4a9de0268c23ae8123fb62b314ea6d6711a7cd
Joseph Schuchart <schuchart@hlrs.de>
2019-02-14 13:18:28 +0100

    Restructure phase handling code to keep active phases in sync

--- dart-impl/tasking/src/internal/dart_tasking_phase.c
+++ dart-impl/tasking/src/internal/dart_tasking_phase.c
@@ -14,95 +14,109 @@ static dart_taskphase_t max_active_phases_mod = INT_MAX;
 static dart_taskphase_t num_active_phases     = 0;
 static dart_taskphase_t num_active_phases_lb  = 0;
 static int32_t *phase_task_counts             = NULL;
+static dart_taskphase_t matching_interval     = INT_MIN;
+static dart_taskphase_t phases_remaining      = INT_MAX;
+static dart_mutex_t     allocation_mutex      = DART_MUTEX_INITIALIZER;
+static float            matching_factor       = 1.0;
+/*
+ * by default, each phase's is 1 but it increases if previous phase(s)
+ * did not see any tasks, i.e., the phase will also release the previous phase(s)
+ */
+static int32_t *phase_task_weight             = NULL;
 static size_t num_units;
 
-
-void
-dart__tasking__phase_advance()
+static void do_matching()
 {
-  static dart_taskphase_t matching_interval = INT_MIN;
-  static dart_taskphase_t phases_remaining  = INT_MAX;
-  static dart_mutex_t     allocation_mutex  = DART_MUTEX_INITIALIZER;
-  static float            matching_factor   = 1.0;
-  if (matching_interval == INT_MIN) {
-    dart__base__mutex_lock(&allocation_mutex);
-    dart_team_size(DART_TEAM_ALL, &num_units);
-    if (num_units == 1) {
-      // disable matching
-      matching_interval = -1;
-    }
-    if (matching_interval == INT_MIN)
-    {
-      matching_interval = dart__base__env__number(
-                            DART_MATCHING_INTERVAL_ENVSTR, -1);
-      if (matching_interval > 0) {
-        phases_remaining = matching_interval;
-        DART_LOG_TRACE("Intermediate task matching enabled: interval %d",
-                      matching_interval);
-        max_active_phases = dart__base__env__number(
-                              DART_MATCHING_PHASE_MAX_ACTIVE_ENVSTR,
-                              3*matching_interval);
-        if (max_active_phases > -1) {
-          if (max_active_phases < matching_interval) {
-            DART_LOG_WARN(
-              "The number of max active phases (%d) is set smaller than the matching "
-              "interval (%d), which will likely lead to a deadlock. Adjusting to 2x "
-              "the matching interval.", max_active_phases, matching_interval);
-            max_active_phases = 2*matching_interval;
-          }
-          // give us some wiggle-room in case not all phases create tasks
-          max_active_phases_mod = 1.2*max_active_phases;
-
-          // if we reach the phase limit, we wait for 50% of them to be processed
-          num_active_phases_lb = dart__base__env__number(
-                                  DART_MATCHING_PHASE_LB_ENVSTR,
-                                  max_active_phases / 2);
-
-          phase_task_counts = calloc(sizeof(*phase_task_counts), max_active_phases_mod);
-        }
-        matching_factor = dart__base__env__float(
-                              DART_MATCHING_PHASE_INTERVAL_FACTOR_ENVSTR,
-                              1);
+  DART_LOG_TRACE("Performing intermediate matching");
+  dart__tasking__perform_matching(creation_phase);
+  if (matching_interval < max_active_phases) {
+    dart_taskphase_t next_interval = matching_interval * matching_factor;
+    if (next_interval > 0) {
+      if (next_interval < max_active_phases) {
+        matching_interval = next_interval;
       } else {
-        DART_LOG_TRACE("Intermediate task matching disabled");
+        matching_interval = max_active_phases;
       }
+    } else {
+      matching_interval = 1;
     }
-    dart__base__mutex_unlock(&allocation_mutex);
+    DART_LOG_TRACE("Next matching interval: %d", matching_interval);
   }
+  phases_remaining = matching_interval;
+}
 
-  // no need to handle phases if there is only one unit
+static void do_init()
+{
+  dart__base__mutex_lock(&allocation_mutex);
+  dart_team_size(DART_TEAM_ALL, &num_units);
   if (num_units == 1) {
-    return;
+    // disable matching
+    matching_interval = -1;
   }
-
-  if (--phases_remaining == 0) {
-    DART_LOG_TRACE("Performing intermediate matching");
-    dart__tasking__perform_matching(creation_phase);
-    if (matching_interval < max_active_phases) {
-      dart_taskphase_t next_interval = matching_interval * matching_factor;
-      if (next_interval > 0) {
-        if (next_interval < max_active_phases) {
-          matching_interval = next_interval;
-        } else {
-          matching_interval = max_active_phases;
+  if (matching_interval == INT_MIN)
+  {
+    matching_interval = dart__base__env__number(
+                          DART_MATCHING_INTERVAL_ENVSTR, -1);
+    if (matching_interval > 0) {
+      phases_remaining = matching_interval;
+      DART_LOG_TRACE("Intermediate task matching enabled: interval %d",
+                    matching_interval);
+      max_active_phases = dart__base__env__number(
+                            DART_MATCHING_PHASE_MAX_ACTIVE_ENVSTR,
+                            3*matching_interval);
+      if (max_active_phases > -1) {
+        if (max_active_phases < matching_interval) {
+          DART_LOG_WARN(
+            "The number of max active phases (%d) is set smaller than the matching "
+            "interval (%d), which will likely lead to a deadlock. Adjusting to 2x "
+            "the matching interval.", max_active_phases, matching_interval);
+          max_active_phases = 2*matching_interval;
         }
-      } else {
-        matching_interval = 1;
-      }
-      DART_LOG_TRACE("Next matching interval: %d", matching_interval);
-    }
-    phases_remaining = matching_interval;
-  }
+        // give us some wiggle-room in case not all phases create tasks
+        max_active_phases_mod = 1.2*max_active_phases;
 
-  ++creation_phase;
+        // if we reach the phase limit, we wait for 50% of them to be processed
+        num_active_phases_lb = dart__base__env__number(
+                                DART_MATCHING_PHASE_LB_ENVSTR,
+                                max_active_phases / 2);
+        if (num_active_phases_lb < matching_interval)
+        {
+          num_active_phases_lb = matching_interval;
+        }
+
+        phase_task_counts = calloc(max_active_phases_mod, sizeof(*phase_task_counts));
+        phase_task_weight = malloc(max_active_phases_mod*sizeof(*phase_task_weight));
+      }
+      matching_factor = dart__base__env__float(
+                            DART_MATCHING_PHASE_INTERVAL_FACTOR_ENVSTR,
+                            1);
+    } else {
+      DART_LOG_TRACE("Intermediate task matching disabled");
+    }
+  }
+  dart__base__mutex_unlock(&allocation_mutex);
+}
+
+static inline void
+wait_for_active_phases(int32_t num_tasks_prev_phase)
+{
   if (phase_task_counts != NULL) {
     // contribute to task execution until we are free to create tasks again
     int entry = creation_phase%max_active_phases_mod;
-    if (DART_FETCH32(&num_active_phases) == max_active_phases
-        || DART_FETCH32(&phase_task_counts[entry]) > 0)
+
+    // default weight is 1
+    phase_task_weight[entry] = 1;
+    if (num_tasks_prev_phase == 0) {
+      // carry over weight from previous phase
+      int prev_phase_entry = (creation_phase - 1)%max_active_phases_mod;
+      phase_task_weight[entry] += phase_task_weight[prev_phase_entry];
+    }
+
+    if (num_active_phases == max_active_phases
+        || phase_task_counts[entry] > 0)
     {
-      while (DART_FETCH32(&num_active_phases) > num_active_phases_lb
-          || DART_FETCH32(&phase_task_counts[entry]) > 0) {
+      while (num_active_phases > num_active_phases_lb
+          || phase_task_counts[entry] > 0) {
         /*
         DART_LOG_TRACE("Waiting for phase %d to become available "
                       "(entry: %d, active phases: %d, in phase %d: %d)",
@@ -119,6 +133,33 @@ dart__tasking__phase_advance()
       DART_FETCH32(&phase_task_counts[entry]),
       entry);
   }
+}
+
+void
+dart__tasking__phase_advance()
+{
+  if (matching_interval == INT_MIN) {
+    do_init();
+  }
+
+  // no need to handle phases if there is only one unit
+  if (num_units == 1) {
+    return;
+  }
+
+  int32_t num_tasks_prev_phase = -1;
+  if (creation_phase >= 0) {
+    num_tasks_prev_phase = phase_task_counts[creation_phase % max_active_phases_mod];
+    DART_ASSERT(num_tasks_prev_phase >= 0);
+  }
+
+  if (--phases_remaining == 0) {
+    do_matching();
+  }
+
+  ++creation_phase;
+  wait_for_active_phases(num_tasks_prev_phase);
+  DART_FETCH_AND_INC32(&num_active_phases);
 }
 
 dart_taskphase_t
@@ -167,7 +208,6 @@ dart__tasking__phase_add_task()
                     &phase_task_counts[creation_phase%max_active_phases_mod]);
     if (val == 0) {
       DART_LOG_TRACE("Phase %d saw its first task!", creation_phase);
-      DART_INC_AND_FETCH32(&num_active_phases);
     }
   }
 }
@@ -176,12 +216,12 @@ void
 dart__tasking__phase_take_task(dart_taskphase_t phase)
 {
   if (NULL != phase_task_counts && phase >= 0) {
-    int32_t val = DART_DEC_AND_FETCH32(
-                    &phase_task_counts[phase%max_active_phases_mod]);
+    int entry = phase%max_active_phases_mod;
+    int32_t val = DART_DEC_AND_FETCH32(&phase_task_counts[entry]);
     DART_LOG_TRACE("Phase %d has %d tasks active!", phase, val);
     if (val == 0) {
       DART_LOG_TRACE("Phase %d is ready for re-use!", phase);
-      DART_DEC_AND_FETCH32(&num_active_phases);
+      DART_SUB_AND_FETCH32(&num_active_phases, phase_task_weight[entry]);
     }
   }
 }
