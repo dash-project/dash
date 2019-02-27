e1f72c427706bbdc0845443cee4bb06980c176e8
Joseph Schuchart <schuchart@hlrs.de>
2019-02-27 22:33:55 +0100

    Extend signal handler output and re-raise signal for immediate abort

--- dart-impl/tasking/src/internal/dart_tasking_signals.c
+++ dart-impl/tasking/src/internal/dart_tasking_signals.c
@@ -5,15 +5,29 @@
 #include <dash/dart/if/dart_initialization.h>
 #include <dash/dart/tasking/dart_tasking_priv.h>
 
+static const char *
+signal_name(int signum)
+{
+  switch(signum){
+    case SIGSEGV: return "Segmentation Fault";
+    case SIGBUS:  return "Bus Error";
+    default:      return "<unknown>";
+  }
+}
 
 static void handler(int signum, siginfo_t *si, void *ptr)
 {
    ucontext_t *uc = (ucontext_t *)ptr;
+   dart_thread_t *thread = dart__tasking__current_thread();
+   DART_LOG_ERROR("Thread %d caught signal %d (%s):\n"
+                  "\taddress: %p\n"
+                  "\tctx: %p\n"
+                  "\tcurrent task: %p\n",
+                  dart__tasking__thread_num(), signum, signal_name(signum),
+                  si->si_addr, uc, thread ? thread->current_task : NULL);
 
-   DART_LOG_ERROR("Thread %d caught signal %d: address %p, ctx %p",
-                  dart__tasking__thread_num(), signum, si->si_addr, uc);
-   dart_abort(signum);
-
+   // re-raise the signal to force shutdown
+   raise(signum);
 }
 
 
