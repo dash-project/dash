009ffd43f9b2ad90e159fab88e84beeb16b55a37
Joseph Schuchart <schuchart@hlrs.de>
2019-05-16 14:34:59 +0200

    TilePattern: properly implement coords() member function

--- dash/include/dash/pattern/TilePattern.h
+++ dash/include/dash/pattern/TilePattern.h
@@ -1417,25 +1417,6 @@ public:
   }
 
   /**
-   * Cartesian index space representing the underlying memory model of the
-   * pattern.
-   *
-   * \see DashPatternConcept
-   */
-  constexpr const MemoryLayout_t & memory_layout() const {
-    return _memory_layout;
-  }
-
-  /**
-   * Cartesian index space representing the underlying local memory model
-   * of this pattern for the calling unit.
-   * Not part of DASH Pattern concept.
-   */
-  constexpr const LocalMemoryLayout_t & local_memory_layout() const {
-    return _local_memory_layout;
-  }
-
-  /**
    * Cartesian arrangement of the Team containing the units to which this
    * pattern's elements are mapped.
    *
@@ -1452,8 +1433,39 @@ public:
    * \see DashPatternConcept
    */
   constexpr std::array<IndexType, NumDimensions> coords(
+    /// Global index (offset) to convert
     IndexType index) const {
-    return _memory_layout.coords(index);
+
+    ::std::array<IndexType, NumDimensions> pos{};
+    auto block_coords = _blockspec.coords(index / _blocksize_spec.size());
+    auto phase_coords = _blocksize_spec.coords(index % _blocksize_spec.size());
+    for (auto d = 0; d < NumDimensions; ++d) {
+      pos[d] = block_coords[d]*_blocksize_spec.extent(d) + phase_coords[d];
+    }
+    return pos;
+  }
+
+  /**
+   * Convert given global linear offset (index) to global cartesian
+   * coordinates.
+   *
+   * \see DashPatternConcept
+   */
+  constexpr std::array<IndexType, NumDimensions> coords(
+    /// Global index (offset) to convert
+    IndexType index,
+    /// View specification (offsets) to apply on \c coords
+    const ViewSpec_t & viewspec) const {
+
+    ::std::array<IndexType, NumDimensions> pos;
+    auto block_coords = _blockspec.coords(index / _blocksize_spec.size(),
+                                          viewspec);
+    auto phase_coords = _blocksize_spec.coords(index % _blocksize_spec.size(),
+                                               viewspec);
+    for (auto d = 0; d < NumDimensions; ++d) {
+      pos[d] = block_coords[d]*_blocksize_spec.extent(d) + phase_coords[d];
+    }
+    return pos;
   }
 
   /**
