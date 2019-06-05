a583798e0310a4b99eadaceb274297bafde2ef63
Joseph Schuchart <schuchart@hlrs.de>
2019-06-05 13:31:44 +0200

    Print a stacktrace inside the signal handler

--- dart-impl/tasking/src/internal/dart_tasking_signals.c
+++ dart-impl/tasking/src/internal/dart_tasking_signals.c
@@ -2,8 +2,12 @@
 #include <signal.h>
 #include <ucontext.h>
 
+#include <execinfo.h>
+
 #include <dash/dart/if/dart_initialization.h>
 #include <dash/dart/tasking/dart_tasking_priv.h>
+
+#define BT_DEPTH 100
 
 static const char *
 signal_name(int signum)
@@ -15,20 +19,36 @@ signal_name(int signum)
   }
 }
 
+static void print_stacktrace()
+{
+  DART_LOG_ERROR("Gathering stacktrace...");
+  void *buffer[BT_DEPTH];
+  int nptrs = backtrace(buffer, BT_DEPTH);
+  char **strings = backtrace_symbols(buffer, nptrs);
+  if (strings == NULL) {
+    DART_LOG_ERROR("Failed to gather symbols for backtrace!");
+  }
+  for (int i = 0; i < nptrs; i++) {
+    DART_LOG_ERROR("\t %s", strings[i]);
+  }
+  free(strings);
+}
+
 static void handler(int signum, siginfo_t *si, void *ptr)
 {
-   ucontext_t *uc = (ucontext_t *)ptr;
-   dart_thread_t *thread = dart__tasking__current_thread();
-   DART_LOG_ERROR("Thread %d caught signal %d (%s):\n"
-                  "\taddress: %p\n"
-                  "\tctx: %p\n"
-                  "\tcurrent task: %p\n",
-                  dart__tasking__thread_num(), signum, signal_name(signum),
-                  si->si_addr, uc, thread ? thread->current_task : NULL);
+  ucontext_t *uc = (ucontext_t *)ptr;
+  dart_thread_t *thread = dart__tasking__current_thread();
+  DART_LOG_ERROR("Thread %d caught signal %d (%s):\n"
+                 "\taddress: %p\n"
+                 "\tctx: %p\n"
+                 "\tcurrent task: %p\n",
+                 dart__tasking__thread_num(), signum, signal_name(signum),
+                 si->si_addr, uc, thread ? thread->current_task : NULL);
 
-   // re-raise the signal to force shutdown
-   // the default signal handler has already been restored via SA_RESETHAND
-   raise(signum);
+  print_stacktrace();
+  // re-raise the signal to force shutdown
+  // the default signal handler has already been restored via SA_RESETHAND
+  raise(signum);
 }
 
 
