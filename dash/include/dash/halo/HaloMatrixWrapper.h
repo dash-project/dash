4c7c337fc1e6bb773904ea524de795102a6fc57b
Joseph Schuchart <schuchart@hlrs.de>
2019-06-05 13:32:43 +0200

    HaloMatrixWrapper: fix async halo update test functions

--- dash/include/dash/halo/HaloMatrixWrapper.h
+++ dash/include/dash/halo/HaloMatrixWrapper.h
@@ -323,17 +323,34 @@ public:
   }
 
   /**
-   * Waits until the halo updates for the given halo region is finished.
+   * Tests for the completion of halo update of the given halo region.
    * Only useful for asynchronous halo updates.
    */
   bool test(region_index_t index) {
     int32_t flag = 1;
     auto it_find = _region_data.find(index);
     if(it_find != _region_data.end())
-      dart_test_local(&it_find->second.halo_data.handle, &flag);
+      dart_test_local(&it_find->second.handle, &flag);
 
     return !(flag == 0);
   }
+
+  /**
+   * Test for completion of the halo updates for all halo regions.
+   * Only useful for asynchronous halo updates.
+   */
+  bool test() {
+    int ret = 1;
+    for(auto& region : _region_data) {
+      if (region.second.handle != DART_HANDLE_NULL) {
+        int flag;
+        dart_test_local(&region.second.handle, &flag);
+        ret &= flag;
+      }
+    }
+    return !(ret == 0);
+  }
+
 
 
   /**
