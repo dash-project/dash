bc6bb38a54c0fcb1746ed1362490d986a68be36f
Patrick <st103061@stud.uni-stuttgart.de>
2019-12-04 15:13:16 +0100

    get_current_task_descr() added.

--- dart-impl/tasking/src/dart_tasking_pthreads.c
+++ dart-impl/tasking/src/dart_tasking_pthreads.c
@@ -177,6 +177,9 @@ dart_task_t * get_current_task();
 static inline
 dart_thread_t * get_current_thread();
 
+static inline
+char * get_current_task_descr();
+
 static
 dart_task_t * next_task(dart_thread_t *thread);
 
@@ -523,6 +526,13 @@ dart_task_t * get_current_task()
 {
   return get_current_thread()->current_task;
 }
+
+static inline
+char * get_current_task_descr()
+{
+  return get_current_thread()->current_task->descr;
+}
+
 
 /**
  * Try to get a task from the thread-local queue.
