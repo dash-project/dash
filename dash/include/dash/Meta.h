391e6268e10afd977aedff9005d6029b44966dfb
Bert Wesarg <bert.wesarg@tu-dresden.de>
2020-03-10 15:31:15 +0100

    SPEC: Avoid defining a `SPEC` macro

--- dash/include/dash/Meta.h
+++ dash/include/dash/Meta.h
@@ -130,10 +130,10 @@ template <typename T>
 struct closure_traits : closure_traits<decltype(&T::operator())> {
 };
 
-#define REM_CTOR(...) __VA_ARGS__
-#define SPEC(cv, var, is_var)                                                 \
+#define DASH_FUNCTIONAL_CLOSURE_TRAILTS_SPEC_REM_CTOR(...) __VA_ARGS__
+#define DASH_FUNCTIONAL_CLOSURE_TRAILTS_SPEC(cv, var, is_var)                 \
   template <typename C, typename R, typename... Args>                         \
-  struct closure_traits<R (C::*)(Args... REM_CTOR var) cv> { /* NOLINT */     \
+  struct closure_traits<R (C::*)(Args... DASH_FUNCTIONAL_CLOSURE_TRAILTS_SPEC_REM_CTOR var) cv> { /* NOLINT */ \
     using arity       = std::integral_constant<std::size_t, sizeof...(Args)>; \
     using is_variadic = std::integral_constant<bool, is_var>;                 \
     using is_const    = std::is_const<int cv>; /* NOLINT */                   \
@@ -144,10 +144,13 @@ struct closure_traits : closure_traits<decltype(&T::operator())> {
     using arg = typename std::tuple_element<i, std::tuple<Args...>>::type;    \
   };
 
-SPEC(const, (, ...), 1)
-SPEC(const, (), 0)
-SPEC(, (, ...), 1)
-SPEC(, (), 0)
+DASH_FUNCTIONAL_CLOSURE_TRAILTS_SPEC(const, (, ...), 1)
+DASH_FUNCTIONAL_CLOSURE_TRAILTS_SPEC(const, (), 0)
+DASH_FUNCTIONAL_CLOSURE_TRAILTS_SPEC(, (, ...), 1)
+DASH_FUNCTIONAL_CLOSURE_TRAILTS_SPEC(, (), 0)
+
+#undef DASH_FUNCTIONAL_CLOSURE_TRAILTS_SPEC
+#undef DASH_FUNCTIONAL_CLOSURE_TRAILTS_SPEC_REM_CTOR
 
 }  // namespace functional
 }  // namespace dash
